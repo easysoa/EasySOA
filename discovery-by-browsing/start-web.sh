b79cccb67568d48d5e4399b29ae8fea17bae74c7
Marwane Kalam-Alami <marwane.ka@gmail.com>
2011-05-06 14:15:16 +0200

    discovery by browsing : demo scenario set up

--- discovery-by-browsing/start-web.sh
+++ discovery-by-browsing/start-web.sh
@@ -3,7 +3,7 @@ LINE="----------------------------------------------------"
 
 echo $LINE
 echo "Service Finder Client + Intranet Web Server"
-echo "(Access from http://localhost:8083 by default)"
+echo "(Access from http://127.0.0.1:8083 by default)"
 echo $LINE
 
 node ./webserver/server.js
