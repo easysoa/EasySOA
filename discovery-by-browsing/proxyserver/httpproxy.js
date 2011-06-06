265572bef77886f1f5130df950e9ff15b9a65cf0
Marwane Kalam-Alami <marwane.ka@gmail.com>
2011-06-06 10:25:35 +0200

    socket.io integration

--- discovery-by-browsing/proxyserver/httpproxy.js
+++ discovery-by-browsing/proxyserver/httpproxy.js
@@ -2,49 +2,12 @@ var http = require('http');
 var fs = require('fs');
 var url = require('url');
 var base64 = require('./tools/base64.js');
+var io = require('./tools/socket.io');
 
 eval(fs.readFileSync('proxyserver/httpproxy-config.js', 'ASCII'));
 
-var status = new Object();
-status.error = null;
-status.foundLinks = new Object();
-
-// Send found WSDLs in JSONP format (or JSON by default)
-function getFoundWSDLs(callback) {
-	var json_string = JSON.stringify(status);
-	if (status.error != null) {
-		status.error = null;
-	}
-	return (callback) ? callback+"("+json_string+");" : json_string;
-}
-
-// Scraper response handling
-function scraperResponse(response) {
-	var data = "";
-	response.on('data', function(chunk) {
-		console.log("received sthg");
-		data += chunk.toString("ascii");
-	});
-	response.on('end', function() {
-		var json = null;
-		try {
-			console.log("result:" +data);
-			json = JSON.parse(data);
-			if (json.foundLinks) {
-				for (link in json.foundLinks) {
-					status.foundLinks[link] = {
-						'url': json.foundLinks[link],
-						'serviceName': link,
-						'applicationName': json.applicationName
-					};			
-					console.log("Found: "+link+" = "+json.foundLinks[link]);
-				}
-			}
-		} catch (err) {
-			console.log("[INFO] Note: "+err.message+" ("+data+")");
-		}
-	});
-}
+var wsdlList = new Array();
+var clients = null;
 
 function responseError(request, response, msg) {
 	console.log("[ERROR] " + msg + " (on request for "+request.url+")");
@@ -58,21 +21,18 @@ var server = http.createServer(function(request, response) {
 
 	var request_url = url.parse(request.url, true);
 
-	// If direct request to proxy, send found WSDLs
-<<<<<<< HEAD
-	if (request.headers['host'] == "127.0.0.1:"+config.proxy_port) {
-=======
+  // Don't allow exceptions
 	if (request.headers['host'] == "localhost:"+config.proxy_port
         || request.headers['host'] == "127.0.0.1:"+config.proxy_port) {
->>>>>>> 2697f9d78e1feee67a294de30a1173f8dea32f5b
 		response.writeHead(200, {
 			"Content-Type": "text/javascript"
 		});
-		response.write(getFoundWSDLs(request_url.query.callback));
 		response.end();
 		return;
 	}
-
+  
+  //// Proxying
+  
 	// Create request to wanted server
 	if (!request_url.port)
 		request_url.port = 80;
@@ -84,7 +44,7 @@ var server = http.createServer(function(request, response) {
 		path : request_url.href,
 		headers : request.headers
 	};
-	
+  
 	// Send request
 	var proxy_request = http.request(proxy_options,
 			function(proxy_response) {
@@ -115,13 +75,15 @@ var server = http.createServer(function(request, response) {
 		proxy_request.end();
 	});
 
+  //// Scraping
+  
 	// Don't scrape ignored paths
 	for (i = 0; i < config.ignore.length; i++) {
 		if (request_url.href.indexOf(config.ignore[i]) != -1) {
 			return;
 		}
 	}
-
+  
 	console.log("[INFO] Scraping: "+request_url.href);
 	
 	// Notification to scrapers
@@ -137,15 +99,43 @@ var server = http.createServer(function(request, response) {
 			host : scraperURL.hostname,
 			path : scraperURL.href.replace('?', request.url)
 		};
-		console.log(scraperOptions);
-
+                    
 		// Scraper request
 		scraper_request = http.request(scraperOptions, function(scraper_response) {
-			scraperResponse(scraper_response);
+			
+      // Scraper response handling
+        var data = "";
+        scraper_response.on('data', function(chunk) {
+          data += chunk.toString("ascii");
+        });
+        scraper_response.on('end', function() {
+          try {
+            var json = JSON.parse(data);
+            if (json.foundLinks) {
+              for (link in json.foundLinks) {
+                var newLink = {
+                      'url': json.foundLinks[link],
+                      'serviceName': link,
+                      'applicationName': json.applicationName
+                    };
+                if (clients != null) {
+                  clients.broadcast(JSON.stringify(newLink));
+                }
+                wsdlList[link] = newLink;
+              }
+            }
+          } catch (err) {
+            console.log("[INFO] Note: "+err.message+" ("+data+")");
+          }
+        });
+        
 		});
 		scraper_request.addListener('error', function(error) {
-			status.error = "Scraper unavailable: "+config.scrapers[scraper];
-			console.log("[WARN] "+status.error);
+		  msg = "Scraper unavailable: "+config.scrapers[scraper];
+		  if (clients != null) {
+  		  clients.broadcast("ERROR: "+msg);
+  		}
+			console.log("[WARN] "+msg);
 		});
 		scraper_request.end();
 		
@@ -153,3 +143,17 @@ var server = http.createServer(function(request, response) {
 });
 
 server.listen(config.proxy_port);
+
+var io = io.listen(server);
+io.on('connection', function(client){
+
+  // Give global access to socket.io connections
+  if (clients == null)
+    clients = client;
+    
+  // Send stored WSDLs
+  for (key in wsdlList) {
+    client.send(JSON.stringify(wsdlList[key]));
+  }
+    
+});
--- discovery-by-browsing/webserver/www/easysoa/core/js/App.js
+++ discovery-by-browsing/webserver/www/easysoa/core/js/App.js
@@ -1,15 +1,34 @@
 $(function() {
 
+	window.socket = new io.Socket(null, {port: 8081, rememberTransport: false});
+  
 	window.AppView = Backbone.View.extend({
 	
 		/**
 		 * Application initialization
 		 */
 		initialize: function() {
+    
 			this.descriptorsView = new DescriptorsView;
 			this.frameView = new FrameView;
 			this.navBarView = new NavbarView;
 			this.submitFormView = new SubmitFormView;
+      
+      socket.connect();
+      socket.on('message', function(obj){
+        if (obj.indexOf("ERROR") == 0) {
+          SubmitForm.failure(obj.substring(7, obj.length-1));
+        }
+        else {
+          obj = jQuery.parseJSON(obj);
+          console.log(obj);
+          if (!Descriptors.contains(obj)) {
+            Descriptors.add(obj);
+          }
+        }
+      });
+      socket.send('hi');
+      
 		}
 	
 	});
