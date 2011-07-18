c6becd23323ad3207b6c9fe3da3fdd2d0e1a3eff
Marwane Kalam-Alami <marwane.ka@gmail.com>
2011-07-18 14:17:31 +0200

    modified dbbrowsing launchers

---
+++ discovery-by-browsing/start-dbbProxy.bat
@@ -0,0 +1,13 @@
+@echo off
+
+set NODE=node
+
+set LINE=----------------------------------------------------
+
+echo %LINE%
+echo HTTP Proxy Server
+echo (Default configuration: Host=127.0.0.1, Port=8081)
+echo %LINE%
+
+rem Default port: 8081
+%NODE% ./proxyserver/httpproxy.js
--- discovery-by-browsing/start-web.bat
+++ discovery-by-browsing/start-web.bat
@@ -2,11 +2,11 @@
 
 set NODE=node
 
-set LINE="----------------------------------------------------"
+set LINE=----------------------------------------------------
 
 echo %LINE%
-echo "Service Finder Client + Intranet Web Server"
-echo "(Access from http://127.0.0.1:8083 by default)"
+echo Service Finder Client + Intranet Web Server
+echo (Access from http://127.0.0.1:8083 by default)
 echo %LINE%
 
 %NODE% ./webserver/server.js
