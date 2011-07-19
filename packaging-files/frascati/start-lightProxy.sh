3edc67a9a52a110b4da697b94ce507af499e58dd
Marc Dutoo <marc.dutoo@openwide.fr>
2011-07-19 10:32:11 +0200

    Setup monitoring proxy as proxy of light(paf) proxy

--- packaging-files/frascati/start-lightProxy.sh
+++ packaging-files/frascati/start-lightProxy.sh
@@ -7,4 +7,5 @@ echo "(Deployed on http://localhost:7001)"
 echo "DEPENDENCY: Running PAF services"
 echo $LINE
 
+CUSTOM_JAVA_OPTS=-Dcxf.config.file=cxfEsperProxy.xml
 ./bin/frascati-easysoa run RestSoapProxy.composite -libpath sca-apps/proxy-1.0-SNAPSHOT.jar
