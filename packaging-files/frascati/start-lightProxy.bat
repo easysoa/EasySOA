8de09d1f3b3cdcd31b18da17469b9dc27efce504
Marwane Kalam-Alami <marwane.ka@gmail.com>
2011-07-21 15:28:34 +0200

    fixed light proxy launcher

--- packaging-files/frascati/start-lightProxy.bat
+++ packaging-files/frascati/start-lightProxy.bat
@@ -8,4 +8,4 @@ echo DEPENDENCY: Running PAF services
 echo %LINE%
 
 set CUSTOM_JAVA_OPTS=-Dcxf.config.file=cxfEsperProxy.xml
-"./bin/frascati-easysoa" run RestSoapProxy.composite -libpath sca-apps/proxy-1.0-SNAPSHOT.jar
+"./bin/frascati-easysoa" run RestSoapProxy_withIntents.composite -libpath sca-apps/proxy-1.0-SNAPSHOT.jar
--- packaging-files/frascati/start-lightProxy.sh
+++ packaging-files/frascati/start-lightProxy.sh
@@ -8,4 +8,4 @@ echo "DEPENDENCY: Running PAF services"
 echo $LINE
 
 CUSTOM_JAVA_OPTS=-Dcxf.config.file=cxfEsperProxy.xml
-./bin/frascati-easysoa run RestSoapProxy.composite -libpath sca-apps/proxy-1.0-SNAPSHOT.jar
+./bin/frascati-easysoa run RestSoapProxy_withIntents.composite -libpath sca-apps/proxy-1.0-SNAPSHOT.jar
