841a032d4894549f71815dfe066eab2c4f711c74
Marwane Kalam-Alami <marwane.ka@gmail.com>
2011-07-22 16:27:45 +0200

    renamed light proxy launcher

--- distrib/frascati/start-lightProxyPaf.sh
+++ distrib/frascati/start-lightProxyPaf.sh
@@ -2,7 +2,7 @@
 LINE="----------------------------------------------------"
 
 echo $LINE
-echo "EasySOA Light service proxy"
+echo "EasySOA Light service proxy for PureAirFlowers"
 echo "(Deployed on http://localhost:7001)"
 echo "DEPENDENCY: Running PAF services"
 echo $LINE
