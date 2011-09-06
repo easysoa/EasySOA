66197cfb4f8321979eaad8da4d7ae5e219858f15
Marwane Kalam-Alami <marwane.ka@gmail.com>
2011-09-06 15:46:32 +0200

    Fixed Travel backup services distribution

--- easysoa-distribution/packaging-files/travelBackup/start-travelBackup.bat
+++ easysoa-distribution/packaging-files/travelBackup/start-travelBackup.bat
@@ -6,4 +6,5 @@ echo Travel services backup
 echo (Deployed on http://localhost:9020)
 echo %LINE%
 
+cd bin
 java -jar easysoa-samples-smarttravel-services-backup-1.0-SNAPSHOT.jar http://localhost:9020/
