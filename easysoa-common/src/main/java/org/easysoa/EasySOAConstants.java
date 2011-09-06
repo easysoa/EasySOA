8a1157b83963156adc5aa65c50eb223e51582a31
Marwane Kalam-Alami <marwane.ka@gmail.com>
2011-09-06 14:56:21 +0200

    Added easysoa-common project

---
+++ easysoa-common/src/main/java/org/easysoa/EasySOAConstants.java
@@ -0,0 +1,16 @@
+package org.easysoa;
+
+/**
+ * This class contains global EasySOA constants, mainly 
+ * used to store the ports used by various components.
+ * 
+ * TODO: Change for a real configuration file.
+ * 
+ * @author mkalam-alami
+ *
+ */
+public class EasySOAConstants {
+
+    public final static int NUXEO_TEST_PORT = 6088;
+    
+}
