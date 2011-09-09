bbbcb3f394e564438b4b2013a315b78f0952abe5
Marc Dutoo <marc.dutoo@openwide.fr>
2011-09-09 16:49:39 +0200

    improved test deps

--- easysoa-registry/easysoa-registry-core/src/main/java/org/easysoa/test/EasySOACoreFeature.java
+++ easysoa-registry/easysoa-registry-core/src/main/java/org/easysoa/test/EasySOACoreFeature.java
@@ -13,7 +13,10 @@ import org.nuxeo.runtime.test.runner.SimpleFeature;
  *
  */
 @Deploy({
-    "org.easysoa.registry.core"
+    "org.easysoa.registry.core",
+	"org.nuxeo.ecm.actions", // required by easysoa-registry-core contrib
+	"org.nuxeo.ecm.platform.forms.layout.client", // required by easysoa-registry-core contrib
+	"org.nuxeo.ecm.platform.query.api" // required by usermanager's PageProvider
 })
 @Features(NuxeoFeatureBase.class)
 public class EasySOACoreFeature extends SimpleFeature {
