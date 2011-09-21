f9d738a06fe4fe35558d5622f8aa1deec324f70e
Marwane Kalam-Alami <marwane.kalam-alami@openwide.fr>
2011-09-21 11:38:46 +0200

    Groups fix

--- easysoa-registry/easysoa-registry-core/src/main/java/org/easysoa/demo/UserInit.java
+++ easysoa-registry/easysoa-registry-core/src/main/java/org/easysoa/demo/UserInit.java
@@ -1,48 +1,37 @@
 package org.easysoa.demo;
 
-import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
 
 import org.apache.commons.logging.Log;
 import org.apache.commons.logging.LogFactory;
-import org.easysoa.services.DocumentService;
 import org.nuxeo.ecm.core.api.ClientException;
 import org.nuxeo.ecm.core.api.DocumentModel;
-import org.nuxeo.ecm.core.api.NuxeoPrincipal;
 import org.nuxeo.ecm.core.api.UnrestrictedSessionRunner;
-import org.nuxeo.ecm.core.api.security.ACE;
-import org.nuxeo.ecm.core.api.security.ACL;
-import org.nuxeo.ecm.core.api.security.ACP;
-import org.nuxeo.ecm.core.api.security.SecurityConstants;
-import org.nuxeo.ecm.core.api.security.impl.ACLImpl;
 import org.nuxeo.ecm.platform.usermanager.UserManager;
 import org.nuxeo.ecm.platform.usermanager.exceptions.GroupAlreadyExistsException;
-import org.nuxeo.ecm.platform.usermanager.exceptions.UserAlreadyExistsException;
 import org.nuxeo.runtime.api.Framework;
 
 /**
- * Users & groups initialisation for the EasySOA demo.
+ * Users & groups initialization for the EasySOA demo.
  * 
  * @author mkalam-alami
  * 
  */
 public class UserInit extends UnrestrictedSessionRunner {
 
-    private static final String GROUP_BUSINESS_USER = "Business user";
+    private static final String GROUP_BUSINESS_USER = "Business User";
     private static final String GROUP_ARCHITECT = "Architect";
     private static final String GROUP_DEVELOPER = "Developer";
     private static final String GROUP_IT_STAFF = "IT Staff";
     private static final String GROUP_ADMINISTRATOR = "Administrator";
     
-    private static final String GROUP_DEFAULT_MEMBERS = "members";
+   // private static final String GROUP_DEFAULT_MEMBERS = "members";
     private static final String GROUP_DEFAULT_ADMINISTRATORS = "administrators";
     
     private static Log log = LogFactory.getLog(UserInit.class);
 
     private UserManager userManager;
-    
-    private DocumentService docService;
 
     public UserInit(String repositoryName) {
         super(repositoryName);
@@ -58,22 +47,22 @@ public class UserInit extends UnrestrictedSessionRunner {
         try {
 
             userManager = Framework.getService(UserManager.class);
-            docService = Framework.getService(DocumentService.class);
             
             // Set new groups as childs of the default "members" group
-            setSubgroups(GROUP_DEFAULT_MEMBERS, new String[] { GROUP_BUSINESS_USER, GROUP_DEVELOPER,
-                    GROUP_ARCHITECT, GROUP_IT_STAFF });
-            setSubgroups(GROUP_DEFAULT_ADMINISTRATORS, new String[] { GROUP_ADMINISTRATOR });
+            addSubgroups(/*XXX: Should be GROUP_DEFAULT_MEMBERS*/ GROUP_DEFAULT_ADMINISTRATORS, 
+                    new String[] { GROUP_BUSINESS_USER, GROUP_DEVELOPER, GROUP_IT_STAFF });
+            addSubgroups(GROUP_DEVELOPER, new String[] { GROUP_ARCHITECT });
+            addSubgroups(GROUP_DEFAULT_ADMINISTRATORS, new String[] { GROUP_ADMINISTRATOR }); // Just to 'rename' the group
 
-            // Set write rights for all "members" members
-            DocumentModel wsRootModel = docService.getWorkspaceRoot(session);
+            // Set write rights for all "members" members (FIXME)
+            /*DocumentModel wsRootModel = session.getRootDocument();
             ACP acp = wsRootModel.getACP();
             ACL acl = new ACLImpl("easysoa-demo-rights");
             acl.add(new ACE(GROUP_DEFAULT_MEMBERS, SecurityConstants.EVERYTHING, true));
             acp.addACL(acl);
             wsRootModel.setACP(acp, true);
             session.saveDocument(wsRootModel);
-            session.save();
+            session.save();*/
 
             log.info("Successfully updated demo groups.");
 
@@ -91,7 +80,7 @@ public class UserInit extends UnrestrictedSessionRunner {
      * @return The new group, or the previously created one if it already exists.
      * @throws ClientException
      */
-    private DocumentModel setSubgroups(String groupName, String[] subGroups) throws ClientException {
+    private DocumentModel addSubgroups(String groupName, String[] subGroups) throws ClientException {
 
         // Create or get group
         DocumentModel groupModel = null;
@@ -104,10 +93,15 @@ public class UserInit extends UnrestrictedSessionRunner {
             groupModel = userManager.getGroupModel(groupName);
         }
 
-        // Set subgroups
-        List<String> subGroupsList = Arrays.asList(subGroups);
+        // Add subgroups
+        List<?> subGroupsListPrev = (List<?>) groupModel.getProperty(userManager.getGroupSchemaName(),
+                userManager.getGroupSubGroupsField());
+        List<String> subGroupsListNew = Arrays.asList(subGroups);
+        for (Object o : subGroupsListPrev) {
+            subGroupsListNew.add((String) o);
+        }
         groupModel.setProperty(userManager.getGroupSchemaName(),
-                userManager.getGroupSubGroupsField(), subGroupsList);
+                userManager.getGroupSubGroupsField(), subGroupsListNew);
 
         // Save changes
         userManager.updateGroup(groupModel);
