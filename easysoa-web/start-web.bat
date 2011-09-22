3b1f289ba25be01b565ef639a7aa2fc8798aad62
Marwane Kalam-Alami <marwane.kalam-alami@openwide.fr>
2011-09-22 17:48:11 +0200

    Minor DBBrowsing improvements

--- easysoa-web/start-web.bat
+++ easysoa-web/start-web.bat
@@ -6,7 +6,9 @@ set LINE=----------------------------------------------------
 
 echo %LINE%
 echo HTTP Web Server
-echo (Default configuration: Host=127.0.0.1, Port=8083)
+echo (Default static server configuration: Host=127.0.0.1, Port=8083
+echo  Default proxy configuration: Host=127.0.0.1, Port=8081)
+echo DEPENDENCIES: Service registry (to log in)
 echo %LINE%
 
 rem Default port: 8083
