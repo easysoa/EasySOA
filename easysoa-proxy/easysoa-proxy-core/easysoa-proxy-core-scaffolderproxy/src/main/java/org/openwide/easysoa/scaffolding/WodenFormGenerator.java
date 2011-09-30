8282623739133fb2c040abd50eb680e4e755f08b
Jeremie Guillemotte <jeremie.guillemotte@openwide.fr>
2011-09-30 10:26:17 +0200

    Cleaning form generator classes, refactoring to remove duplicate code. EasyWSDl is the default WSDL parser because we have a library version problem with Woden.

--- easysoa-proxy/easysoa-proxy-core/easysoa-proxy-core-scaffolderproxy/src/main/java/org/openwide/easysoa/scaffolding/WodenFormGenerator.java
+++ easysoa-proxy/easysoa-proxy-core/easysoa-proxy-core-scaffolderproxy/src/main/java/org/openwide/easysoa/scaffolding/WodenFormGenerator.java
@@ -1,9 +1,13 @@
 package org.openwide.easysoa.scaffolding;
 
+import java.io.File;
+import java.net.URI;
 import java.util.ArrayList;
 import java.util.Iterator;
 import java.util.List;
-
+import javax.xml.parsers.DocumentBuilder;
+import javax.xml.parsers.DocumentBuilderFactory;
+import org.apache.log4j.Logger;
 import org.apache.woden.WSDLFactory;
 import org.apache.woden.WSDLReader;
 import org.apache.woden.tool.converter.Convert;
@@ -21,10 +25,17 @@ import org.openwide.easysoa.scaffolding.wsdltemplate.WSEndpoint;
 import org.openwide.easysoa.scaffolding.wsdltemplate.WSOperation;
 import org.osoa.sca.annotations.Property;
 import org.osoa.sca.annotations.Scope;
+import org.w3c.dom.Document;
 
 @Scope("COMPOSITE")
 public class WodenFormGenerator implements TemplateFormGeneratorInterface {
 
+	/**
+	 * Logger
+	 */
+	private static Logger logger = Logger.getLogger(WodenFormGenerator.class.getClass());	
+	
+	// Description of the WSDL file to parse
 	private Description wsdlDescription;
 	
 	// Just for compatibily with airport tuto hack
@@ -33,7 +44,7 @@ public class WodenFormGenerator implements TemplateFormGeneratorInterface {
 	
 	@Override
 	public void setWsdl(String wsdlXmlSource) throws Exception {
-		System.out.println("Entering in setWsdl method : " + wsdlXmlSource);
+		logger.debug("Entering in setWsdl method : " + wsdlXmlSource);
 		try{
 			// Factory and reader
 			WSDLFactory factory = WSDLFactory.newInstance();
@@ -43,37 +54,36 @@ public class WodenFormGenerator implements TemplateFormGeneratorInterface {
 			//reader.setFeature(WSDLReader.FEATURE_VALIDATION, true);
 			
 			// Converter
-			// TODO add a test to check the version of the WSDL to transform
-			// If version is 1.0 or 1.1, need to convert it to 2.0 for Woden
-			//if(){
+			// Check the WSDl version : if version 1.0 or 1.1 => transformation to WSDL 2.0 is required
+			if(!isWsdl2(wsdlXmlSource)){
 				Convert convert = new Convert();
 				String newTargetNS = null;
-				String targetDir = "/home/jguillemotte/tests";;
+				String targetDir = System.getProperty("user.dir");
 				boolean verbose = true;
 				boolean overwrite = true; 
-				
 				String convertFile = convert.convertFile(newTargetNS,wsdlXmlSource,targetDir,verbose,overwrite);
-				System.out.println(convertFile);
-	
-				wsdlDescription = reader.readWSDL(targetDir + "/" + convertFile);
-			/*}
+				logger.debug(convertFile);
+				logger.debug(targetDir + "/" + convertFile);
+				String wsdl2read = targetDir + "/" + convertFile;
+				wsdlDescription = reader.readWSDL(wsdl2read);
+				System.out.println("Bug : " + wsdlDescription);
+			}
 			else {
 				wsdlDescription = reader.readWSDL(wsdlXmlSource);
-			}*/
-				
+			}
 		}
 		catch(Exception ex){
 			ex.printStackTrace();
 			throw ex;
 		}
-		System.out.println("WSDL loaded !");
+		logger.debug("WSDL loaded !");
 	}
 
 	@Override
 	public String getServiceName() {
-		System.out.println("Entering in getServiceName method");
+		logger.debug("Entering in getServiceName method");
 		String serviceName = wsdlDescription.getServices()[0].getName().getLocalPart();
-		System.out.println("Service name : " + serviceName);
+		logger.debug("Service name : " + serviceName);
 		return serviceName;
 	}
 
@@ -94,11 +104,10 @@ public class WodenFormGenerator implements TemplateFormGeneratorInterface {
 
 	@Override
 	public List<WSOperation> getOperations(WSEndpoint wsEndpoint) {
-		System.out.println("Entering in getOperations");
+		logger.debug("Entering in getOperations");
 		ArrayList<WSOperation> wsOperationList = new ArrayList<WSOperation>();
 		Endpoint endpoint = wsdlDescription.getServices()[0].getEndpoint(new NCName(wsEndpoint.getName()));
 		for(BindingOperation operation : endpoint.getBinding().getBindingOperations()){
-			//bindingOperationList.add(operation);
 			wsOperationList.add(new WSOperation(operation.getInterfaceOperation().getName()));
 		}
 		return wsOperationList;
@@ -106,111 +115,104 @@ public class WodenFormGenerator implements TemplateFormGeneratorInterface {
 
 	@Override
 	public String getOperationName(WSOperation wsOperation) {
-		System.out.println("Entering in getOperationName");
+		logger.debug("Entering in getOperationName");
 		/*return operation.getInterfaceOperation().getName().getLocalPart();*/
 		return wsOperation.getName();
 	}
 
 	@Override
 	public String getOutputMessageName(WSEndpoint wsEndpoint, WSOperation wsOperation) {
-		System.out.println("Entering in getOutputMessageName");
-		Endpoint endpoint = wsdlDescription.getServices()[0].getEndpoint(new NCName(wsEndpoint.getName()));
-		for(BindingOperation operation : endpoint.getBinding().getBindingOperations()){
-			if(operation.getInterfaceOperation().getName().equals(wsOperation.getQName())){
-				InterfaceMessageReference[] messages = operation.getInterfaceOperation().getInterfaceMessageReferences();
-				for(int i = 0; i <messages.length; i++){
-					if(Direction.OUT.equals(messages[i].getDirection())){
-						System.out.println("OUT message found");
-						return messages[i].getElementDeclaration().getName().getLocalPart();
-					}
-				}
+		logger.debug("Entering in getOutputMessageName");
+		BindingOperation operation = getBindingOperation(wsEndpoint, wsOperation);
+		InterfaceMessageReference[] messages = operation.getInterfaceOperation().getInterfaceMessageReferences();
+		for(int i = 0; i <messages.length; i++){
+			if(Direction.OUT.equals(messages[i].getDirection())){
+				logger.debug("OUT message found");
+				return messages[i].getElementDeclaration().getName().getLocalPart();
 			}
 		}
 		return "";
-		/*
-		InterfaceMessageReference[] messages = bindingOperation.getInterfaceOperation().getInterfaceMessageReferences();
-		System.out.println("messages : " + messages.length);
-		for(int i = 0; i <messages.length; i++){
-			if(Direction.OUT.equals(messages[i].getDirection())){
-				System.out.println("OUT message found");
-				return messages[i].getElementDeclaration().getName().getLocalPart();
-			}
-		}*/
 	}
 
 	@Override
 	public List<String> getInputFields(WSEndpoint wsEndpoint, WSOperation wsOperation) {
-		ArrayList<String> inputFields = new ArrayList<String>();
-		System.out.println("Entering in getOutputMessageName");
-		Endpoint endpoint = wsdlDescription.getServices()[0].getEndpoint(new NCName(wsEndpoint.getName()));
-		for(BindingOperation operation : endpoint.getBinding().getBindingOperations()){
-			if(operation.getInterfaceOperation().getName().equals(wsOperation.getQName())){
-				InterfaceMessageReference[] messages = operation.getInterfaceOperation().getInterfaceMessageReferences();
-				for(int i = 0; i <messages.length; i++){
-					if(Direction.IN.equals(messages[i].getDirection())){
-						System.out.println("API to use : " + messages[i].getElementDeclaration().getContentModel());
-						System.out.println("Object type : " + messages[i].getElementDeclaration().getContent());
-						XmlSchemaElement schemaElement =  (XmlSchemaElement) (messages[i].getElementDeclaration().getContent());
-						System.out.println("Type name : " + schemaElement.getName());
-						//System.out.println(".... " + schemaElement.getConstraints());
-						//System.out.println(".... " + schemaElement.getSchemaType());
-						//System.out.println(".... " + schemaElement.getSchemaType().getDataType());
-						//System.out.println(".... " + wsdlDescription.getElementDeclaration(schemaElement.getSchemaType().getQName()).getContent());			 	
-						XmlSchemaElement elem = (XmlSchemaElement) (wsdlDescription.getElementDeclaration(schemaElement.getSchemaType().getQName()).getContent());
-						XmlSchemaComplexType elemComplexType = (XmlSchemaComplexType)elem.getSchemaType();
-						XmlSchemaSequence sequence = (XmlSchemaSequence)(elemComplexType.getParticle());
-						Iterator<XmlSchemaSequenceMember> iter = sequence.getItems().iterator();
-						while(iter.hasNext()){
-							Object obj = iter.next();
-							if(obj instanceof XmlSchemaElement){
-								XmlSchemaElement field = (XmlSchemaElement) obj;
-								System.out.println("Field found = " + field.getName());
-								inputFields.add(field.getName());
-							}
-						}
-					}
-				}
-			}
-		}
-		return inputFields;
+		logger.debug("Entering in getOutputMessageName");
+		BindingOperation operation = getBindingOperation(wsEndpoint, wsOperation);
+		return getFields(operation, Direction.IN);
 	}
 
 	@Override
 	public List<String> getOutputFields(WSEndpoint wsEndpoint, WSOperation wsOperation) {
-		ArrayList<String> outputFields = new ArrayList<String>();
-		System.out.println("Entering in getOutputMessageName");
+		logger.debug("Entering in getOutputMessageName");
+		BindingOperation operation = getBindingOperation(wsEndpoint, wsOperation);				
+		return getFields(operation, Direction.OUT);
+	}
+	
+	/**
+	 * Return the binding corresponding to the endpoint and the operation 
+	 * @param wsEndpoint
+	 * @param wsOperation
+	 * @return The binding, null if the binding is not found
+	 */
+	private BindingOperation getBindingOperation(WSEndpoint wsEndpoint, WSOperation wsOperation){
 		Endpoint endpoint = wsdlDescription.getServices()[0].getEndpoint(new NCName(wsEndpoint.getName()));
 		for(BindingOperation operation : endpoint.getBinding().getBindingOperations()){
 			if(operation.getInterfaceOperation().getName().equals(wsOperation.getQName())){
-				InterfaceMessageReference[] messages = operation.getInterfaceOperation().getInterfaceMessageReferences();
-				System.out.println("messages : " + messages.length);
-				for(int i = 0; i <messages.length; i++){
-					if(Direction.OUT.equals(messages[i].getDirection())){
-						//return messages[i].getElementDeclaration().getName().getLocalPart();
-						System.out.println("API to use : " + messages[i].getElementDeclaration().getContentModel());
-						System.out.println("Object type : " + messages[i].getElementDeclaration().getContent());
-						XmlSchemaElement schemaElement =  (XmlSchemaElement) (messages[i].getElementDeclaration().getContent());				
-					 	//System.out.println("Type name : " + schemaElement.getName());
-					 	//System.out.println(".... " + schemaElement.getConstraints().getCount());
-					 	//System.out.println(".... " + schemaElement.getSchemaType());
-					 	//System.out.println(".... " + wsdlDescription.getElementDeclaration(schemaElement.getSchemaType().getQName()).getContent());
-						XmlSchemaElement elem = (XmlSchemaElement) (wsdlDescription.getElementDeclaration(schemaElement.getSchemaType().getQName()).getContent());
-						XmlSchemaComplexType elemComplexType = (XmlSchemaComplexType)elem.getSchemaType();
-						XmlSchemaSequence sequence = (XmlSchemaSequence)(elemComplexType.getParticle());
-						Iterator<XmlSchemaSequenceMember> iter = sequence.getItems().iterator();
-						while(iter.hasNext()){
-							Object obj = iter.next();
-							if(obj instanceof XmlSchemaElement){
-								XmlSchemaElement field = (XmlSchemaElement) obj;
-								System.out.println("Field found = " + field.getName());
-								outputFields.add(field.getName());
-							}
-						}
+				return operation;
+			}
+		}
+		return null;
+	}
+	
+	/**
+	 * Returns a list of fields corresponding to the operation and the message direction
+	 * @param operation The operation for which to get the I/O fields
+	 * @param direction The message direction
+	 * @return A string list of fields 
+	 */
+	private List<String> getFields(BindingOperation operation, Direction direction){
+		ArrayList<String> outputFields = new ArrayList<String>();
+		InterfaceMessageReference[] messages = operation.getInterfaceOperation().getInterfaceMessageReferences();		
+		logger.debug("messages : " + messages.length);
+		for(int i = 0; i <messages.length; i++){
+			if(direction.equals(messages[i].getDirection())){
+				XmlSchemaElement schemaElement =  (XmlSchemaElement) (messages[i].getElementDeclaration().getContent());				
+				XmlSchemaElement elem = (XmlSchemaElement) (wsdlDescription.getElementDeclaration(schemaElement.getSchemaType().getQName()).getContent());
+				XmlSchemaComplexType elemComplexType = (XmlSchemaComplexType)elem.getSchemaType();
+				XmlSchemaSequence sequence = (XmlSchemaSequence)(elemComplexType.getParticle());			 	
+				Iterator<XmlSchemaSequenceMember> iter = sequence.getItems().iterator();
+				//Iterator<Object> iter = sequence.getItems().getIterator();
+				while(iter.hasNext()){
+					Object obj = iter.next();
+					if(obj instanceof XmlSchemaElement){
+						XmlSchemaElement field = (XmlSchemaElement) obj;
+						logger.debug("Field found = " + field.getName());
+						outputFields.add(field.getName());
 					}
 				}
 			}
 		}
-		return outputFields;
+		return outputFields;		
+	}
+	
+	/**
+	 * Return true is the WSDl version is 2.0, false othervise
+	 * @param xmlSource The WSDL File to check
+	 * @return True if the document version is 2.0, false otherwise 
+	 * @throws Exception If a problem occurs
+	 */
+	private boolean isWsdl2(String xmlSource) throws Exception {
+		File file = new File(new URI(xmlSource));
+		DocumentBuilderFactory dbf = DocumentBuilderFactory.newInstance();
+		DocumentBuilder db = dbf.newDocumentBuilder();
+		Document doc = db.parse(file);
+		doc.getDocumentElement().normalize();
+		if("definitions".equalsIgnoreCase(doc.getDocumentElement().getNodeName().substring(doc.getDocumentElement().getNodeName().indexOf(":")+1))){
+			return false;
+		}
+		else {
+			return true;
+		}
 	}
 
 }
