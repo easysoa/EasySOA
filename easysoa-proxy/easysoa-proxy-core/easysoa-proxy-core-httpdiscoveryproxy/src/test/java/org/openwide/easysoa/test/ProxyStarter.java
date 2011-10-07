51b0544a56cf82251dac8c76743969189100e88c
Marc Dutoo <marc.dutoo@openwide.fr>
2011-10-07 16:50:39 +0200

    icompleted httpdiscovery starter test

--- easysoa-proxy/easysoa-proxy-core/easysoa-proxy-core-httpdiscoveryproxy/src/test/java/org/openwide/easysoa/test/ProxyStarter.java
+++ easysoa-proxy/easysoa-proxy-core/easysoa-proxy-core-httpdiscoveryproxy/src/test/java/org/openwide/easysoa/test/ProxyStarter.java
@@ -1,7 +1,11 @@
 package org.openwide.easysoa.test;
 
 import org.apache.log4j.Logger;
+import org.json.JSONException;
+import org.junit.AfterClass;
+import org.junit.BeforeClass;
 import org.junit.Test;
+import org.ow2.frascati.util.FrascatiException;
 
 /**
  * HTTP Discovery Proxy Test Starter.
@@ -13,6 +17,29 @@ public class ProxyStarter extends AbstractProxyTestStarter {
 	 * Logger
 	 */
 	private static Logger logger = Logger.getLogger(ProxyStarter.class.getClass());    
+
+	/**
+	 * Initialize one time the remote systems for the test
+	 * FraSCAti and HTTP discovery Proxy ...
+	 * @throws FrascatiException, InterruptedException 
+	 * @throws JSONException 
+	 */
+    @BeforeClass
+	public static void setUp() throws FrascatiException, InterruptedException, JSONException {
+	   logger.info("Launching FraSCAti and HTTP Discovery Proxy");
+	   startFraSCAti();
+	   startHttpDiscoveryProxy("src/main/resources/httpDiscoveryProxy.composite");
+    }
+	
+    /**
+     * Stop FraSCAti components
+     * @throws FrascatiException
+     */
+    @AfterClass
+    public static void cleanUp() throws FrascatiException{
+    	logger.info("Stopping FraSCAti...");
+    	stopFraSCAti();
+    }
     
 	/**
 	 * This test do nothing, just wait for a user action to stop the proxy. 
