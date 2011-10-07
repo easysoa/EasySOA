b10cebcfebcbd037d8a5d40acf170c2a4a1a382c
Jeremie Guillemotte <jeremie.guillemotte@openwide.fr>
2011-10-07 18:16:48 +0200

    HTML form send now a POST request with JSON data structure. It is now possible to add more features to the form.

--- easysoa-proxy/easysoa-proxy-core/easysoa-proxy-core-scaffolderproxy/src/main/java/org/openwide/easysoa/scaffolding/ScaffoldingProxy.java
+++ easysoa-proxy/easysoa-proxy-core/easysoa-proxy-core-scaffolderproxy/src/main/java/org/openwide/easysoa/scaffolding/ScaffoldingProxy.java
@@ -1,44 +1,47 @@
-/**
- * 
- */
 package org.openwide.easysoa.scaffolding;
 
-import javax.ws.rs.GET;
+import javax.servlet.http.HttpServletRequest;
+import javax.ws.rs.POST;
 import javax.ws.rs.Path;
-import javax.ws.rs.PathParam;
 import javax.ws.rs.core.Context;
-import javax.ws.rs.core.Request;
 import javax.ws.rs.core.Response;
-import javax.ws.rs.core.UriInfo;
+
+import org.apache.http.protocol.HttpContext;
 
 /**
+ * Scaffolding proxy interface
  * @author jguillemotte
  *
  */
 public interface ScaffoldingProxy {
 
 	/**
-	 * Receive a REST request, transform it to a SOAP request, get the response and send back the answers to the HTML form
+	 * Receive a REST POST request containing a JSON data structure, transform it to a SOAP request, get the response and send back the answers to the HTML form
 	 * 
-	 * Example of REST request : http://localhost:7001/callService/SoapServiceMockSoapBinding/getPrice/?arg0=patatoes&arg1=25&wsdlUrl=http://localhost:8086/soapServiceMock
+	 * Example of REST request : http://localhost:7001/callService/SoapServiceMockSoapBinding/getPrice/
 	 * where :
 	 * - Binding is SoapServiceMockSoapBinding
 	 * - Operation is getPrice
-	 * - There is 2 parameters for the operation getPrice : arg1 and arg2
-	 * - The WSDL to use must be specified with the wsdlUrl parameter
+	 *
+	 * Here is a sample of JSON structure :
+	 * {
+	 *     "wsRequest": { "service": "SoapServiceMock", "binding": "SoapServiceMockSoapBinding", "operation": "getPrice", "wsdlUrl": "http://localhost:8086/soapServiceMock?wsdl"},
+	 *     "formParameters":[{"paramName":"arg0","paramValue":"test"}, {"paramName":"arg1","paramValue":"25"}]
+	 * }
+	 * 
+	 * - wsRequest contains all informations used directly by the scaffolding proxy
+	 * - formParameters is an array of parameters contained in the HTML form 
 	 * 
 	 * The port is specified in the scaffoldingProxy.composite file. 
 	 * 	
-	 * @param ui Context UriInfo
-	 * @param request Context Request
-	 * @param binding Binding to call
-	 * @param operation Operation to call
+	 * @param httpContext HTTP context
+	 * @param request Servlet context
 	 * @return The SOAP service response converted to JSON format
 	 * @throws Exception If a problem occurs
 	 */
-	@GET
+	@POST
 	@Path("/callService/{binding}/{operation}/")
-	public Response redirectRestToSoap(@Context UriInfo ui, @Context Request request, @PathParam("binding") String binding, @PathParam("operation") String operation) throws Exception;	
+	public Response redirectRestToSoap(@Context HttpContext httpContext, @Context HttpServletRequest request);
 	
 }
 
