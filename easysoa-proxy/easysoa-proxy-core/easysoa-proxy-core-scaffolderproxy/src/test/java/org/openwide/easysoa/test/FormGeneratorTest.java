e85546f91a860ced52896337faf858071df61378
Marc Dutoo <marc.dutoo@openwide.fr>
2011-10-07 11:22:46 +0200

    separated test & starter / tester

---
+++ easysoa-proxy/easysoa-proxy-core/easysoa-proxy-core-scaffolderproxy/src/test/java/org/openwide/easysoa/test/FormGeneratorTest.java
@@ -0,0 +1,70 @@
+package org.openwide.easysoa.test;
+
+import static org.junit.Assert.*;
+
+import java.io.IOException;
+
+import org.apache.http.client.ClientProtocolException;
+import org.apache.http.client.ResponseHandler;
+import org.apache.http.client.methods.HttpGet;
+import org.apache.http.impl.client.BasicResponseHandler;
+import org.apache.http.impl.client.DefaultHttpClient;
+import org.apache.log4j.Logger;
+import org.easysoa.EasySOAConstants;
+import org.junit.BeforeClass;
+import org.junit.Test;
+import org.junit.matchers.JUnitMatchers;
+import org.ow2.frascati.util.FrascatiException;
+
+public class FormGeneratorTest extends AbstractTest {
+
+	/**
+	 * Logger
+	 */
+	private static Logger logger = Logger.getLogger(FormGeneratorTest.class.getClass());
+    
+	/**
+	 * Init the remote systems for the test
+	 * Frascati and HTTP Proxy
+	 * Instantiate FraSCAti and retrieve services.
+	 * @throws InterruptedException
+	 */
+	@BeforeClass
+	public static void setUp() throws FrascatiException, InterruptedException {
+		// Start fraSCAti
+		startFraSCAti();
+		// Start the soap service mock
+		startSoapServiceMockComposite();
+		// Start Scaffolding Proxy test
+		startScaffoldingProxyComposite();
+	}	
+
+	/**
+	 * Test the HTML form generation
+	 * @throws Exception If a problem occurs
+	 */
+	@Test
+	public void testFormGenerator() throws Exception {
+		ResponseHandler<String> responseHandler = new BasicResponseHandler();
+		// Send a request to generate the HTML form
+		DefaultHttpClient httpProxyClient = new DefaultHttpClient();
+		String form = httpProxyClient.execute(new HttpGet("http://localhost:" + EasySOAConstants.HTML_FORM_GENERATOR_PORT + "/scaffoldingProxy/?wsdlUrl=http://localhost:8086/soapServiceMock?wsdl"), responseHandler);
+		logger.debug("response = " + form);
+		// Compare it to a registered one
+		assertThat(form, JUnitMatchers.containsString("Service : SoapServiceMock"));		
+		assertThat(form, JUnitMatchers.containsString("<u>getPrice</u>"));
+		//assertThat(form, JUnitMatchers.containsString("<input class=\"outputField\" id=\"return_ordersNumber\" disabled name=\"ordersNumber\" type=\"text\">"));		
+	}
+
+	@Test
+	public final void testRestSoapProxy() throws ClientProtocolException, IOException{
+		ResponseHandler<String> responseHandler = new BasicResponseHandler();
+		// Send a request to test the rest/soap proxy
+		DefaultHttpClient httpProxyClient = new DefaultHttpClient();
+		String resp = httpProxyClient.execute(new HttpGet("http://localhost:" + EasySOAConstants.REST_SOAP_PROXY_PORT +"/callService/SoapServiceMockSoapBinding/getPrice/?arg0=patatoes&arg1=25&wsdlUrl=http://localhost:8086/soapServiceMock"), responseHandler);
+		logger.debug("response = " + resp);		
+		// Compare the result with the expected one
+		assertEquals("{\"Body\":{\"getPriceResponse\":{\"return\":\"250.0\"}}}", resp);
+	}
+	
+}
--- easysoa-proxy/easysoa-proxy-core/easysoa-proxy-core-scaffolderproxy/src/test/java/org/openwide/easysoa/test/FormGeneratorTester.java
+++ easysoa-proxy/easysoa-proxy-core/easysoa-proxy-core-scaffolderproxy/src/test/java/org/openwide/easysoa/test/FormGeneratorTester.java
@@ -1,21 +1,13 @@
 package org.openwide.easysoa.test;
 
-import static org.junit.Assert.assertEquals;
-import static org.junit.Assert.assertThat;
 import java.io.IOException;
+
 import javax.xml.soap.SOAPException;
-import org.apache.http.client.ClientProtocolException;
-import org.apache.http.client.ResponseHandler;
-import org.apache.http.client.methods.HttpGet;
-import org.apache.http.impl.client.BasicResponseHandler;
-import org.apache.http.impl.client.DefaultHttpClient;
+
 import org.apache.log4j.Logger;
 import org.junit.BeforeClass;
-import org.junit.Ignore;
 import org.junit.Test;
-import org.junit.matchers.JUnitMatchers;
 import org.ow2.frascati.util.FrascatiException;
-import org.easysoa.EasySOAConstants;
 
 public class FormGeneratorTester extends AbstractTest {
 
@@ -38,36 +30,6 @@ public class FormGeneratorTester extends AbstractTest {
 		startSoapServiceMockComposite();
 		// Start Scaffolding Proxy test
 		startScaffoldingProxyComposite();
-	}	
-
-	/**
-	 * Test the HTML form generation
-	 * @throws Exception If a problem occurs
-	 */
-	@Test
-	@Ignore
-	public void testFormGenerator() throws Exception {
-		ResponseHandler<String> responseHandler = new BasicResponseHandler();
-		// Send a request to generate the HTML form
-		DefaultHttpClient httpProxyClient = new DefaultHttpClient();
-		String form = httpProxyClient.execute(new HttpGet("http://localhost:" + EasySOAConstants.HTML_FORM_GENERATOR_PORT + "/scaffoldingProxy/?wsdlUrl=http://localhost:8086/soapServiceMock?wsdl"), responseHandler);
-		logger.debug("response = " + form);
-		// Compare it to a registered one
-		assertThat(form, JUnitMatchers.containsString("Service : SoapServiceMock"));		
-		assertThat(form, JUnitMatchers.containsString("<h3>Operations :</h3><br/>\n								<u>getPrice</u>"));
-		//assertThat(form, JUnitMatchers.containsString("<input class=\"outputField\" id=\"return_ordersNumber\" disabled name=\"ordersNumber\" type=\"text\">"));		
-	}
-
-	@Test
-	@Ignore
-	public final void testRestSoapProxy() throws ClientProtocolException, IOException{
-		ResponseHandler<String> responseHandler = new BasicResponseHandler();
-		// Send a request to test the rest/soap proxy
-		DefaultHttpClient httpProxyClient = new DefaultHttpClient();
-		String resp = httpProxyClient.execute(new HttpGet("http://localhost:" + EasySOAConstants.REST_SOAP_PROXY_PORT +"/callService/SoapServiceMockSoapBinding/getPrice/?arg0=patatoes&arg1=25&wsdlUrl=http://localhost:8086/soapServiceMock"), responseHandler);
-		logger.debug("response = " + resp);		
-		// Compare the result with the expected one
-		assertEquals("{\"Body\":{\"getPriceResponse\":{\"return\":\"250.0\"}}}", resp);
 	}
 	
 	/**
