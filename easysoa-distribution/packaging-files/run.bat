c0c61368e15444eb8172a409c6c2af4a2a573254
Marwane Kalam-Alami <marwane.kalam-alami@openwide.fr>
2011-10-10 16:20:40 +0200

    Distribution fixes

--- easysoa-distribution/packaging-files/run.bat
+++ easysoa-distribution/packaging-files/run.bat
@@ -14,22 +14,17 @@ cd ..\..
 
 rem esper
 cd frascati
-start "EasySOA Demo - Esper Proxy" start-esperProxy.bat 2>&1 ^| "../tee.exe" ..\log\esperProxy.log
+start "EasySOA Demo - Esper Proxy" start-esperProxy.bat 2>&1 ^| "..\tee.exe" ..\log\esperProxy.log
 cd ..
 
 rem pafServices
 cd pafServices
-start "EasySOA Demo - Web Services" start-pafServices.bat 2>&1 ^| "../tee.exe" ..\log\pafServices.log
+start "EasySOA Demo - Web Services" start-pafServices.bat 2>&1 ^| "..\tee.exe" ..\log\pafServices.log
 cd ..
 
 rem travelBackup
 cd travelBackup
-start "EasySOA Demo - Travel Services Backup" start-travelBackup.bat 2>&1 ^| "../tee.exe" ..\log\travelBackup.log
-cd ..
-
-rem dbbProxy
-cd dbbProxy
-start "EasySOA Demo - Web Proxy" start-dbbProxy.bat 2>&1 ^| "../tee.exe" ..\log\dbbProxy.log
+start "EasySOA Demo - Travel Services Backup" start-travelBackup.bat 2>&1 ^| "..\tee.exe" ..\log\travelBackup.log
 cd ..
 
 rem sleep 3 (let the servers start, see http://stackoverflow.com/questions/1672338/how-to-sleep-for-5-seconds-in-windowss-command-prompt-or-dos)
@@ -37,25 +32,25 @@ ping -n 4 127.0.0.1 > nul
 
 rem travel
 cd frascati
-start "EasySOA Demo - Travel" start-travelDemo.bat 2>&1 ^| "../tee.exe" ..\log\travelDemo.log
+start "EasySOA Demo - Travel" start-travelDemo.bat 2>&1 ^| "..\tee.exe" ..\log\travelDemo.log
 cd ..
 
 rem airportService
-cd talend/airportService/SimpleProvider
-start "EasySOA Demo - Talend Airport Service" SimpleProvider_run.bat 2>&1 ^| "../tee.exe" ..\log\airportService.log
-cd ../../..
+cd talend\airportService\SimpleProvider
+start "EasySOA Demo - Talend Airport Service" SimpleProvider_run.bat 2>&1 ^| "..\..\..\tee.exe" ..\..\..\log\airportService.log
+cd ..\..\..
 
 rem sleep 7 (let the demo start)
 ping -n 8 127.0.0.1 > nul
 
 rem web
 cd web
-start "EasySOA Demo - Web Server" start-web.bat 2>&1 ^| "../tee.exe" ..\log\web.log
+start "EasySOA Demo - Web Server" start-web.bat 2>&1 ^| "..\tee.exe" ..\log\web.log
 cd ..
 
 rem uiScaffolder
 cd frascati
-start "EasySOA Demo - UI Scaffolder Proxy" start-uiScaffolder.bat 2>&1 ^| "../tee.exe" ..\log\uiScaffolder.log
+start "EasySOA Demo - UI Scaffolder Proxy" start-uiScaffolder.bat 2>&1 ^| "..\tee.exe" ..\log\uiScaffolder.log
 cd ..
 
 rem sleep 2
