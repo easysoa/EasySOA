4ff47ca13040401395db2e0bee976e1702298130
Marwane Kalam-Alami <marwane.kalam-alami@openwide.fr>
2011-10-13 10:19:51 +0200

    Minor 'updateVersion' script improvements

--- easysoa-distribution/updateVersion.sh
+++ easysoa-distribution/updateVersion.sh
@@ -8,18 +8,26 @@
 #   (removes all created .versionsBackup files)
 
 if [ "$1" == "clean" ]; then
+
+	# Remove all .versionsBackup files
 	cd ..
 	find ./ -type f -name \*.versionsBackup | xargs rm
 	echo "ok"
 	cd "easysoa-distribution"
+	
 else
 
-	NEW_VERSION=$1
-
+	# Clean repository
 	rm -rf easysoa/
 	cd ..
+	mvn clean
+	
+	# Init
+	NEW_VERSION=$1
 	ROOT_DIR="$( cd "$( dirname "$0" )" && pwd )"
 
+	# Update pom function
+	# (takes a pom path as parameter)
 	update_pom() {
 		pom=$1
 		cd ${pom%pom.xml}
@@ -27,9 +35,12 @@ else
 		cd $ROOT_DIR
 	}
 
+	# Pom search and update
 	for pom in `find -name pom.xml`; do
 		update_pom $pom
 	done
+	
+	# End
 	cd "$ROOT_DIR/easysoa-distribution"
 
 fi
