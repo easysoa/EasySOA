dd22cf05e9807cf31913e484bf87ade19f360296
Marc Dutoo <marc.dutoo@openwide.fr>
2011-10-19 18:55:08 +0200

    patched template header mailing list email

--- easysoa-distribution/licenses/include_all_headers.sh
+++ easysoa-distribution/licenses/include_all_headers.sh
@@ -1,5 +1,6 @@
 #! /bin/bash
 
+# 20111011 add license headers & scripts
 #./include_header.sh ../../easysoa-common "*.java" lgpl3_java_header.txt "EasySOA Common"
 #./include_header.sh ../../easysoa-registry "*.java" lgpl3_java_header.txt "EasySOA Registry"
 #./include_header.sh ../../easysoa-proxy "*.java" lgpl3_java_header.txt "EasySOA Proxy"
@@ -7,3 +8,13 @@
 #./include_header.sh ../../samples/easysoa-samples-pureairflowers "*.java" lgpl3_java_header.txt "EasySOA Samples - PureAirFlowers"
 #./include_header.sh ../../samples/easysoa-samples-smarttravel "*.java" lgpl3_java_header.txt "EasySOA Samples - Smart Travel"
 #./include_header.sh ../../samples/Talend-Airport-Service "*.TODO" TODO_header.txt "EasySOA Samples - Talend Airport"
+
+# 20111019 patch header mailing list email
+#sed -i s/easysoa-dev@groups.google.com/easysoa-dev@googlegroups.com/ ./*header.txt
+#find ../../easysoa-common -name "*.java" -exec sed -i s/easysoa-dev@groups.google.com/easysoa-dev@googlegroups.com/ {} \; -print |more
+#find ../../easysoa-registry -name "*.java" -exec sed -i s/easysoa-dev@groups.google.com/easysoa-dev@googlegroups.com/ {} \; -print |more
+#find ../../easysoa-proxy -name "*.java" -exec sed -i s/easysoa-dev@groups.google.com/easysoa-dev@googlegroups.com/ {} \; -print |more
+#sed -i s/easysoa-dev@groups.google.com/easysoa-dev@googlegroups.com/ ../../easysoa-web/js/web*.js
+#find ../../samples/easysoa-samples-pureairflowers -name "*.java" -exec sed -i s/easysoa-dev@groups.google.com/easysoa-dev@googlegroups.com/ {} \; -print |more
+#find ../../samples/easysoa-samples-smarttravel -name "*.java" -exec sed -i s/easysoa-dev@groups.google.com/easysoa-dev@googlegroups.com/ {} \; -print |more
+
