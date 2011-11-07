dd1db60c913a3244266653507b52a10ebdaeb02f
Marwane Kalam-Alami <marwane.kalam-alami@openwide.fr>
2011-11-07 12:39:31 +0100

    Put clone() in Object prototype

--- mocks/unifiedapi/utils.js
+++ mocks/unifiedapi/utils.js
@@ -7,26 +7,29 @@
 
 /**
  * Clones an object
- * Copied from: http://stackoverflow.com/questions/728360/copying-an-object-in-javascript
+ * Customized from: http://stackoverflow.com/questions/728360/copying-an-object-in-javascript
  */
-function clone(obj) {
-    if (null == obj || "object" != typeof obj) return obj;
-    var copy = obj.constructor();
-    for (var attr in obj) {
-        if (obj.hasOwnProperty(attr)) copy[attr] = obj[attr];
+Object.defineProperty(Object.prototype, "clone", {
+    enumerable : false,
+    value : function() {
+        var copy = this.constructor();
+        for (var attr in this) {
+            if (this.hasOwnProperty(attr))
+                copy[attr] = this[attr];
+        }
+        return copy;
     }
-    return copy;
-}
+});
 
 /**
  * Allows to create an object by extending an existing one.
  * Customized from: http://onemoredigit.com/post/1527191998/extending-objects-in-node-js
  */
 Object.defineProperty(Object.prototype, "extend", {
-    enumerable: false,
-    value: function(from) {
+    enumerable : false,
+    value : function(from) {
         var props = Object.getOwnPropertyNames(from);
-        var dest = clone(this);
+        var dest = this.clone();
         props.forEach(function(name) {
             if (name in dest) {
                 var destination = Object.getOwnPropertyDescriptor(from, name);
