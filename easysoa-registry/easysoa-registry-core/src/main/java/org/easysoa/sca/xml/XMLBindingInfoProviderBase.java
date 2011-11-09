d08a0d9123cf41b210fe974a2aa7e61e62a9f69e
Marwane Kalam-Alami <marwane.kalam-alami@openwide.fr>
2011-11-09 16:21:05 +0100

    Finished fixing mail adress

--- easysoa-registry/easysoa-registry-core/src/main/java/org/easysoa/sca/xml/XMLBindingInfoProviderBase.java
+++ easysoa-registry/easysoa-registry-core/src/main/java/org/easysoa/sca/xml/XMLBindingInfoProviderBase.java
@@ -15,9 +15,8 @@
  * You should have received a copy of the GNU Lesser General Public License
  * along with this program.  If not, see <http://www.gnu.org/licenses/>.
  * 
- * Contact : easysoa-dev@groups.google.com
+ * Contact : easysoa-dev@googlegroups.com
  */
-
 
 package org.easysoa.sca.xml;
 
