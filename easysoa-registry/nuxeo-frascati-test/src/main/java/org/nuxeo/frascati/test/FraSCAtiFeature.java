051bdd53357d524acbb5fcc35b77b6860eae77d1
christophe-m <christophe.munilla@inria.fr>
2011-12-12 16:07:05 +0100

    - introduce FraSCAtiFeature for test - test nuxeo-frascati-module - use maven-dependency-plugin to build the list of FraSCati's runtime dependencies

---
+++ easysoa-registry/nuxeo-frascati-test/src/main/java/org/nuxeo/frascati/test/FraSCAtiFeature.java
@@ -0,0 +1,83 @@
+package org.nuxeo.frascati.test;
+
+import java.io.File;
+import java.io.IOException;
+import java.util.logging.Logger;
+
+import org.nuxeo.common.utils.FileUtils;
+import org.nuxeo.frascati.api.FraSCAtiServiceItf;
+import org.nuxeo.runtime.test.WorkingDirectoryConfigurator;
+import org.nuxeo.runtime.test.runner.Deploy;
+import org.nuxeo.runtime.test.runner.Features;
+import org.nuxeo.runtime.test.runner.FeaturesRunner;
+import org.nuxeo.runtime.test.runner.RuntimeFeature;
+import org.nuxeo.runtime.test.runner.RuntimeHarness;
+import org.nuxeo.runtime.test.runner.SimpleFeature;
+
+@Features(RuntimeFeature.class)
+@Deploy({
+	"org.nuxeo.runtime.bridge",
+	"org.nuxeo.frascati"
+})
+public class FraSCAtiFeature extends SimpleFeature implements WorkingDirectoryConfigurator {
+	
+	 	protected static final Logger log = Logger.getLogger(
+	 			FraSCAtiFeature.class.getCanonicalName());
+	    
+	    protected FraSCAtiServiceItf frascatiService;	   
+		
+	    public void initialize(FeaturesRunner runner) {
+	    	runner.getFeature(RuntimeFeature.class).getHarness().addWorkingDirectoryConfigurator(this);	    	
+	    }
+
+		@Override
+		public void configure(RuntimeHarness harness, File workingDir)
+				throws Exception {
+			
+	    	char sep = File.separatorChar;	
+	    	String frascatiTestLibsPath = null;
+	    	try{
+		    	String testPath = workingDir.getAbsolutePath();			    	
+		    	frascatiTestLibsPath = new StringBuilder(testPath).append(sep).append(
+		    			"frascati").append(sep).append("lib").toString();
+	    	} catch(Exception e){
+	    		e.printStackTrace();
+	    	}
+	    	File frascatiTestLibsDir = new File(frascatiTestLibsPath);
+	    	
+	    	if(!frascatiTestLibsDir.exists()){
+	        	
+	    		frascatiTestLibsDir.mkdirs();
+
+		    	String home = workingDir.getAbsolutePath();	
+		    	log.info("Default environment home path: " + home);
+		    	
+		    	File frascatiConfigDir = new File(new StringBuilder(home).append(sep).append("config").toString());
+		    	frascatiConfigDir.mkdir();	
+		    	
+		    	File configFileSrc = new File("src/test/resources/frascati_boot.properties");
+		    	File configFileDst = new File(new StringBuilder(frascatiConfigDir.getAbsolutePath()).append(
+		    					sep).append("frascati_boot.properties").toString());
+
+		    	try {
+					FileUtils.copy(configFileSrc,configFileDst);
+				} catch (IOException e) {
+					e.printStackTrace();
+				}
+		    	
+		    	File frascatiLibsDir = new File("./src/test/resources/frascati/lib");
+		    	File[] libs = frascatiLibsDir.listFiles();	
+		    	
+		    	for(File srclib : libs){
+		    		String libName = srclib.getName();
+		    		File destlib = new File(new StringBuilder(frascatiTestLibsPath).append(sep).append(libName).toString());
+		    		try {
+						FileUtils.copy(srclib,destlib);
+					} catch (IOException e) {
+						e.printStackTrace();
+					}
+		    	}		    	
+	    	}  		
+		}
+	 	    
+}
---
+++ easysoa-registry/nuxeo-frascati-test/src/test/java/org/nuxeo/frascati/test/TestFraSCAtiInNuxeo.java
@@ -0,0 +1,110 @@
+package org.nuxeo.frascati.test;
+
+import static junit.framework.Assert.assertNotNull;
+import static org.junit.Assert.assertEquals;
+
+import java.io.File;
+import java.net.MalformedURLException;
+import java.net.URL;
+import java.util.logging.Logger;
+
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.nuxeo.ecm.core.api.ClientException;
+import org.nuxeo.frascati.api.AbstractProcessingContext;
+import org.nuxeo.frascati.api.FraSCAtiServiceItf;
+import org.nuxeo.frascati.api.FraSCAtiCompositeItf;
+import org.nuxeo.frascati.NuxeoFraSCAtiException;
+import org.ow2.frascati.util.reflect.ReflectionHelper;
+import org.nuxeo.runtime.api.Framework;
+import org.nuxeo.runtime.test.runner.Features;
+import org.nuxeo.runtime.test.runner.FeaturesRunner;
+
+@RunWith(FeaturesRunner.class)
+@Features(FraSCAtiFeature.class)
+public class TestFraSCAtiInNuxeo {
+
+	private final String scaFilePath = "src/test/resources/" + "helloworld-pojo.composite";
+	
+	protected static final Logger log = Logger.getLogger(TestFraSCAtiInNuxeo.class.getCanonicalName());
+	
+	private final String STARTED = "STARTED";
+	private final String STOPPED = "STOPPED";
+	private FraSCAtiCompositeItf fcomponent = null;
+
+	FraSCAtiServiceItf frascatiService = null;
+	
+	@Before
+	public void setUp() throws MalformedURLException, ClientException, Exception{
+		
+		frascatiService = Framework.getLocalService(FraSCAtiServiceItf.class);
+		assertNotNull(frascatiService);
+		
+		File scaFile = new File(scaFilePath);   
+		
+		frascatiService.processComposite(scaFile.getAbsolutePath(),
+					new ProcessingContextTest(
+							frascatiService.newProcessingContext()));			
+		fcomponent = frascatiService.getComposite("helloworld-pojo");	
+	}
+	
+	@After
+	public void tearDown() throws NuxeoFraSCAtiException{		
+		frascatiService.remove("helloworld-pojo");
+		fcomponent = null;
+	}
+	
+	/**
+     * Test if an existing service can be retrieved 
+     * 
+     * @throws NuxeoFraSCAtiException
+     */
+    @Test
+    public void testGetRunnableServiceAndExecuteIt() throws NuxeoFraSCAtiException {  
+    	Runnable r = frascatiService.getService(fcomponent.getComponent(), "r", Runnable.class);
+	    assertNotNull(r);
+	    r.run();   	
+    	
+    }
+    
+    /**
+     * Test NuxeoFraSCAtiException is thrown if a service is unknown
+     * 
+	 * @throws NuxeoFraSCAtiException
+     */
+    @Test(expected = NuxeoFraSCAtiException.class)
+    public void testThrowExceptionIfGetUnexistingService() throws NuxeoFraSCAtiException {      	
+    	Runnable r = frascatiService.getService(fcomponent.getComponent(), "unknown", Runnable.class);    	
+    }
+   
+    @Test
+    public void testSCACompositeLifeCycle(){    	    	
+    	String state = fcomponent.getState();
+    	if(STARTED.equals(state)){    		
+    		fcomponent.stop();
+    		log.info( fcomponent.getState()); 
+    		assertEquals(STOPPED,fcomponent.getState());
+    		fcomponent.start();
+    		log.info(fcomponent.getState()); 
+    		assertEquals(STARTED,fcomponent.getState());
+    	} else {    		
+    		fcomponent.start();
+    		log.info(fcomponent.getState()); 
+    		assertEquals(STARTED,fcomponent.getState());
+    		fcomponent.stop();
+    		log.info(fcomponent.getState()); 
+    		assertEquals(STOPPED,fcomponent.getState());
+    	}
+    }
+	
+   private class ProcessingContextTest extends AbstractProcessingContext {
+
+    	public ProcessingContextTest(ReflectionHelper delegate) throws NuxeoFraSCAtiException {
+    		super(delegate);
+    	}
+
+    }
+	
+}
