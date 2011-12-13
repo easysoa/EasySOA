37e85d2abb9fbc6f88c531f3eca427c0905b366c
Jeremie Guillemotte <jeremie.guillemotte@openwide.fr>
2011-12-13 10:54:33 +0100

    little changes

--- easysoa-proxy/easysoa-proxy-core/easysoa-proxy-core-httpdiscoveryproxy/src/main/java/org/easysoa/records/replay/ExchangeReplayServiceImpl.java
+++ easysoa-proxy/easysoa-proxy-core/easysoa-proxy-core-httpdiscoveryproxy/src/main/java/org/easysoa/records/replay/ExchangeReplayServiceImpl.java
@@ -1,16 +1,20 @@
 package org.easysoa.records.replay;
 
 import java.util.ArrayList;
+import java.util.Collection;
 import java.util.List;
 import javax.ws.rs.GET;
 import javax.ws.rs.Path;
 import javax.ws.rs.PathParam;
 import javax.ws.rs.Produces;
+import javax.ws.rs.core.MediaType;
 import org.apache.log4j.Logger;
 import org.easysoa.records.ExchangeRecord;
+import org.easysoa.records.RecordCollection;
 import org.easysoa.records.ExchangeRecordStore;
 import org.easysoa.records.ExchangeRecordStoreManager;
 import org.easysoa.records.ExchangeRecordStoreFactory;
+import org.easysoa.records.StoreCollection;
 import org.osoa.sca.annotations.Scope;
 import com.openwide.easysoa.message.OutMessage;
 import com.openwide.easysoa.util.RequestForwarder;
@@ -27,16 +31,20 @@ import com.openwide.easysoa.util.RequestForwarder;
  * - by doing a diff (on server or client) and displaying it
  * - LATER by running asserts gotten from correlator
  * 
+ * 
+ * REST HOWTO
+ * 
+ * How to return collections in jaxrs / jaxb / cxf (without going to Spring conf) : create a strong-typed collection class ex. ExchangeRecords
+ * http://dhruba.name/2008/12/08/rest-service-example-using-cxf-22-jax-rs-10-jaxb-and-spring/#comment-781
+ * NB. resteasy provides additional non-standard annotations for that : @Wrapped http://stackoverflow.com/questions/6192389/root-element-name-in-collections-returned-by-resteasy
+ * 
+ * How to handle a reference to another object not as an object but as its id : XmlJavaTypeAdapter
+ * http://docs.oracle.com/javase/6/docs/api/javax/xml/bind/annotation/adapters/XmlJavaTypeAdapter.html
+ * 
  * @author jguillemotte
  *
  */
 // TODO REST JAXRS service, web UI
-
-/**
- * ExchangeReplayService implementation
- * @author jguillemotte
- *
- */
 @Scope("composite")
 public class ExchangeReplayServiceImpl implements ExchangeReplayService {
 	
@@ -48,28 +56,43 @@ public class ExchangeReplayServiceImpl implements ExchangeReplayService {
 
 	@Override
 	@GET
-	@Path("/list/{storeName}")
-	@Produces("application/json")
-	public List<ExchangeRecord> getExchangeRecordlist(@PathParam("storeName") String exchangeRecordStoreName) {
-		logger.debug("list method called ...");
+	@Path("/getExchangeRecordlist/{storeName}")
+	@Produces({MediaType.APPLICATION_JSON, MediaType.APPLICATION_XML})
+	//public List<ExchangeRecord> getExchangeRecordlist(@PathParam("storeName") String exchangeRecordStoreName) {
+	public RecordCollection getExchangeRecordlist(@PathParam("storeName") String exchangeRecordStoreName) {
+		logger.debug("getExchangeRecordlist method called for store : " + exchangeRecordStoreName);
     	ExchangeRecordStoreManager erfs;
     	List<ExchangeRecord> recordList;
 		try {
 			erfs = ExchangeRecordStoreFactory.createExchangeRecordStore();
 			recordList = erfs.getExchangeRecordlist(exchangeRecordStoreName);
-			logger.debug("recordedList size = " + recordList.size());
 		} catch (Exception ex) {
 			logger.error("An error occurs during the listing of exchanges records", ex);
 			recordList = new ArrayList<ExchangeRecord>();
 		}
-		return recordList;
+		logger.debug("recordedList size = " + recordList.size());
+		return new RecordCollection(recordList);
 	}
 
 	@Override
-	public List<ExchangeRecordStore> getExchangeRecordStorelist() {
-		logger.debug("list{service} method called ...");
+	@GET
+	@Path("/getExchangeRecordlist/{storeName}/{exchangeID}")
+	@Produces({MediaType.APPLICATION_JSON, MediaType.APPLICATION_XML})
+	//public List<ExchangeRecord> getExchangeRecordlist(@PathParam("storeName") String exchangeRecordStoreName) {
+	public RecordCollection getExchangeRecord(@PathParam("storeName") String exchangeRecordStoreName, @PathParam("exchangeID") String exchnageID) {
+		
+	}
+	
+	
+	@Override
+	@GET
+	@Path("/getExchangeRecordStorelist")
+	@Produces({MediaType.APPLICATION_JSON, MediaType.APPLICATION_XML})
+	//public List<ExchangeRecordStore> getExchangeRecordStorelist() {
+	public StoreCollection getExchangeRecordStorelist() {
+		logger.debug("getExchangeRecordStorelist method called ...");
     	ExchangeRecordStoreManager erfs;
-    	List<ExchangeRecordStore> storeList; 
+    	List<ExchangeRecordStore> storeList;
     	try{
     		erfs = ExchangeRecordStoreFactory.createExchangeRecordStore();
     		storeList = erfs.getExchangeRecordStorelist();
@@ -78,15 +101,15 @@ public class ExchangeReplayServiceImpl implements ExchangeReplayService {
 			logger.error("An error occurs during the listing of exchanges record stores", ex);
 			storeList = new ArrayList<ExchangeRecordStore>();    		
     	}
-    	return storeList;
+    	return new StoreCollection(storeList);
 	}	
 	
 	@Override
+	@Path("/replay")	
 	@Produces("application/json")
 	public String replay(@PathParam("exchangeRecordStoreName") String exchangeRecordStoreName, @PathParam("exchangeRecordId") String exchangeRecordId) {
 		// call remote service using chosen record :
 		// see how to share monit.forward(Message) code (extract it in a Util class), see also scaffolder client
-		
 		// NB. without correlated asserts, test on response are impossible,
 		// however diff is possible (on server or client)
 		// ex. on server : http://code.google.com/p/java-diff-utils/
@@ -95,11 +118,12 @@ public class ExchangeReplayServiceImpl implements ExchangeReplayService {
     	ExchangeRecordStoreManager erfs;
 		StringBuffer responseBuffer = new StringBuffer();    	
 		try {
-			List<ExchangeRecord> recordList;
+			//List<ExchangeRecord> recordList;
+			Collection<ExchangeRecord> recordList;
 			erfs = ExchangeRecordStoreFactory.createExchangeRecordStore();
 			// get the record
 			if(exchangeRecordId==null || "".equals(exchangeRecordId)){
-				 recordList = getExchangeRecordlist(exchangeRecordStoreName);
+				recordList = getExchangeRecordlist(exchangeRecordStoreName).getRecords();
 			} else {
 				recordList = new ArrayList<ExchangeRecord>();
 				recordList.add(erfs.load(exchangeRecordStoreName, exchangeRecordId));			
