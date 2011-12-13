14c1eecdee0497c671c954fcddf7f9c3e068858c
Marwane Kalam-Alami <marwane.kalam-alami@openwide.fr>
2011-12-13 12:14:11 +0100

    Made nuxeo-frascati use the resources JAR through the system scope

---
+++ easysoa-registry/easysoa-registry-api/easysoa-registry-api-frascati/src/main/java/org/ow2/frascati/util/FrascatiException.java
@@ -0,0 +1,13 @@
+package org.ow2.frascati.util;
+
+/**
+ * XXX: Has to be replaced by the class from the 'frascati-util-comp' bundle.
+ * 
+ * @author mkalam-alami
+ *
+ */
+public class FrascatiException extends Exception {
+
+    private static final long serialVersionUID = 1L;
+
+}
