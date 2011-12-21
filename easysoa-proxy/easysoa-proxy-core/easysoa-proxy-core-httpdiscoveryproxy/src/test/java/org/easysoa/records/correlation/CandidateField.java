b493e03487d22fff8f3a1a7599a0e929c838a097
Marc Dutoo <marc.dutoo@openwide.fr>
2011-12-21 17:48:46 +0100

    tested correlation between http request & response (REST or SOAP and xml or js styles)

---
+++ easysoa-proxy/easysoa-proxy-core/easysoa-proxy-core-httpdiscoveryproxy/src/test/java/org/easysoa/records/correlation/CandidateField.java
@@ -0,0 +1,62 @@
+package org.easysoa.records.correlation;
+
+//TODO
+public class CandidateField {
+    
+    // kind : id ?
+    // annot : found in GET, count... ??
+
+    private String kind;
+    private String path;
+    private String name;
+    private String value;
+    private String type;
+    private CandidateField finerCandidate;// ??
+    private String valueGetExpression;
+    private String valueSetExpression;
+    
+    public CandidateField(String path, String value) {
+        this.kind = "json"; // "path", "content" ??
+        this.path = path;
+        this.name = this.path.substring(this.path.lastIndexOf('/') + 1);
+        this.value = value;
+    }
+    public CandidateField(String path, String value, String type) {
+        this(path, value);
+        this.type = type;
+    }
+    
+    public String getValue(Object message) {
+        return null;
+    }
+    //public void setValue(String value, Object message) {
+    //    
+    //}
+    public void templatize(Object message) {
+        
+    }
+
+    public String getKind() {
+        return kind;
+    }
+
+    public String getPath() {
+        return path;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public String getValue() {
+        return value;
+    }
+
+    public String getType() {
+        return type;
+    }
+    
+    public String toString() {
+        return "[" + this.getPath() + "=" + this.getValue() + "]";
+    }
+}
