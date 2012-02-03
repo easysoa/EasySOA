8cb2f72d88791728f2cc8ec628c14209e9dd5283
Marwane Kalam-Alami <marwane.kalam-alami@openwide.fr>
2012-02-03 16:38:33 +0100

    Make the distribution use the embedded node executable

---
+++ easysoa-distribution/packaging-files/web/start-web.bat
@@ -0,0 +1,16 @@
+@echo off
+
+set NODE=node
+
+set LINE=----------------------------------------------------
+
+echo %LINE%
+echo HTTP Web Server
+echo (Default static server configuration: Host=127.0.0.1, Port=8083
+echo  Default proxy configuration: Host=127.0.0.1, Port=8081)
+echo DEPENDENCIES: Service registry (to log in)
+echo %LINE%
+
+rem Default port: 8083
+cd js
+../../node/node.exe easysoa.js
