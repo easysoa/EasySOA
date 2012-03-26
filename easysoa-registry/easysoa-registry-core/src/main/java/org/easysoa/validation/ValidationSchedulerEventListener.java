2fe545e4f5d30e03f5674c66ffc4ead39360df92
Marwane Kalam-Alami <marwane.kalam-alami@openwide.fr>
2012-03-26 12:56:57 +0200

    Add empty validation report template

--- easysoa-registry/easysoa-registry-core/src/main/java/org/easysoa/validation/ValidationSchedulerEventListener.java
+++ easysoa-registry/easysoa-registry-core/src/main/java/org/easysoa/validation/ValidationSchedulerEventListener.java
@@ -1,6 +1,15 @@
 package org.easysoa.validation;
 
 
+import java.io.File;
+import java.io.IOException;
+import java.io.StringWriter;
+import java.net.URL;
+import java.util.Date;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
 import org.apache.commons.logging.Log;
 import org.apache.commons.logging.LogFactory;
 import org.easysoa.services.DocumentService;
@@ -10,12 +19,18 @@ import org.nuxeo.ecm.core.api.ClientException;
 import org.nuxeo.ecm.core.api.CoreInstance;
 import org.nuxeo.ecm.core.api.CoreSession;
 import org.nuxeo.ecm.core.api.DocumentModel;
+import org.nuxeo.ecm.core.api.impl.blob.StringBlob;
 import org.nuxeo.ecm.core.api.repository.Repository;
 import org.nuxeo.ecm.core.api.repository.RepositoryManager;
 import org.nuxeo.ecm.core.event.Event;
 import org.nuxeo.ecm.core.event.EventListener;
 import org.nuxeo.runtime.api.Framework;
+import org.nuxeo.runtime.management.Resource;
 import org.nuxeo.runtime.transaction.TransactionHelper;
+
+import freemarker.template.Configuration;
+import freemarker.template.DefaultObjectWrapper;
+import freemarker.template.Template;
 
 /**
  * 
@@ -24,6 +39,13 @@ import org.nuxeo.runtime.transaction.TransactionHelper;
  */
 public class ValidationSchedulerEventListener implements EventListener {
 
+	private static final String TEMPLATES_FOLDER = "./nxserver/nuxeo.war/templates";
+	private static final String VALIDATION_REPORT_TEMPLATE = "validationReport.ftl";
+
+    private static Log log = LogFactory.getLog(ValidationSchedulerEventListener.class);
+    
+	private static Configuration freemarkerCfg = null;
+    
 	private ServiceValidationService serviceValidationService;
 
 	private PublicationService publicationService;
@@ -32,8 +54,16 @@ public class ValidationSchedulerEventListener implements EventListener {
 	
 	private CoreSession session = null;
 	
-    private static Log log = LogFactory.getLog(ValidationSchedulerEventListener.class);
-	
+    static {
+    	try {
+        	freemarkerCfg = new Configuration();
+			freemarkerCfg.setDirectoryForTemplateLoading(new File(TEMPLATES_FOLDER));
+	    	freemarkerCfg.setObjectWrapper(new DefaultObjectWrapper());
+		} catch (IOException e) {
+			log.error("Failed to initialize templating configuration, no validation reports will be produced.", e);
+		}
+    }
+    
 	public ValidationSchedulerEventListener() throws Exception {
 		serviceValidationService = Framework.getService(ServiceValidationService.class);
 		publicationService = Framework.getService(PublicationService.class);
@@ -80,10 +110,30 @@ public class ValidationSchedulerEventListener implements EventListener {
 						session.removeDocument(tmpWorkspaceModel.getRef());
 					}
 				}
-				
-				// Create report
-				DocumentModel workspaceModel = docService.findWorkspace(session, environmentName);
-				// TODO
+
+				if (freemarkerCfg != null) {
+					// Create report
+			        Template tpl = freemarkerCfg.getTemplate(VALIDATION_REPORT_TEMPLATE);
+			        Map<String, Object> params = new HashMap<String, Object>();
+			        params.put("user", "Bob");
+			        StringWriter writer = new StringWriter();
+			        tpl.process(params, writer);
+			        writer.flush();
+			        String reportAsString = writer.getBuffer().toString();
+	
+					// Save report
+					DocumentModel workspaceModel = docService.findWorkspace(session, environmentName);
+					@SuppressWarnings("unchecked")
+					List<Map<String, Object>> files = (List<Map<String, Object>>) workspaceModel.getProperty("files", "files");
+					Map<String, Object> newFile = new HashMap<String, Object>();
+					newFile.put("filename", "Validation report - " + new Date(System.currentTimeMillis()).toString());
+					newFile.put("file", new StringBlob(reportAsString));
+	
+					files.add(newFile);
+					workspaceModel.setProperty("files", "files", files);
+					session.saveDocument(workspaceModel);
+					
+				}
 			
 			}
 			else {
