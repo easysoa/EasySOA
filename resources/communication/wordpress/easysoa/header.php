38d706dfeeb29387c7dc7ebe5f5c2c43324c6085
Marwane Kalam-Alami <marwane.kalam-alami@openwide.fr>
2012-05-02 12:29:32 +0200

    Update theme backup

--- resources/communication/wordpress/easysoa/header.php
+++ resources/communication/wordpress/easysoa/header.php
@@ -1,7 +1,6 @@
 <!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Strict//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-strict.dtd">
 <html xmlns="http://www.w3.org/1999/xhtml" xml:lang="en" lang="en">
 <head>
-  <script type="text/javascript" src="/scripts/emailpage.js"></script>
   <script type="text/javascript" src="<?php bloginfo('template_url'); ?>/js/dropdown.js"></script>
   <meta http-equiv="content-type" content="text/html; charset=<?php bloginfo('charset'); ?>" />
   <meta name="description" content="<?php bloginfo('name'); ?> - <?php bloginfo('description'); ?>" />
