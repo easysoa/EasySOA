ec22219b391b287ec8fa69df3f5f69c290fe9fa4
Marwane Kalam-Alami <marwane.kalam-alami@openwide.fr>
2012-05-11 12:20:51 +0200

    Fix Jetty-based filter test

--- easysoa-registry/easysoa-registry-rest-miner/src/test/java/org/easysoa/records/filters/test/JettyServletFilterTest.java
+++ easysoa-registry/easysoa-registry-rest-miner/src/test/java/org/easysoa/records/filters/test/JettyServletFilterTest.java
@@ -25,60 +25,49 @@ package org.easysoa.records.filters.test;
 
 import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertTrue;
+import static org.mockito.Matchers.any;
+import static org.mockito.Mockito.spy;
+import static org.mockito.Mockito.times;
+import static org.mockito.Mockito.verify;
+
+import javax.servlet.FilterChain;
+import javax.servlet.ServletRequest;
+import javax.servlet.ServletResponse;
 
 import org.easysoa.records.filters.ExchangeRecordServletFilterImpl;
+import org.easysoa.test.TestServlet;
 import org.junit.After;
 import org.junit.Before;
 import org.junit.Test;
-import org.mortbay.jetty.servlet.DefaultServlet;
+import org.mortbay.jetty.servlet.FilterHolder;
 import org.mortbay.jetty.testing.HttpTester;
 import org.mortbay.jetty.testing.ServletTester;
-
-/*
-import org.easysoa.test.EasySOACoreTestFeature;
-import org.easysoa.test.EasySOARepositoryInit;
-import org.junit.runner.RunWith;
-import org.nuxeo.ecm.core.test.annotations.BackendType;
-import org.nuxeo.ecm.core.test.annotations.RepositoryConfig;
-import org.nuxeo.frascati.test.FraSCAtiFeature;
-import org.nuxeo.runtime.test.runner.Features;
-import org.nuxeo.runtime.test.runner.FeaturesRunner;
-*/
 
 /**
  * Class to test the Exchange record servlet filter
  * 
- * @author jguillemotte
+ * @author jguillemotte, mkalam-alami
  *
  */
-//@RunWith(FeaturesRunner.class)
-//@Features({EasySOACoreTestFeature.class,FraSCAtiFeature.class})
-//@RepositoryConfig(type=BackendType.H2, user = "Administrator", init=EasySOARepositoryInit.class)
 public class JettyServletFilterTest {
 
-    // Add a dependency to Easysoa-registry-frascati ???
-    
-    // Start Nuxeo
-    // Start embeded FraSCAti services
-    // Plug the ExchangeRecordServletFilter
-    // Send a test request
-    // Check if a new run with exchange record is recorded
-    
     /*
      * Using Jetty ServletTester is OK for simple ServletFilters tests
      * Here we have to tests more complicated filters using FraSCati/Nuxeo features
      */
     
-    //
     private ServletTester tester;
+
+    private FilterHolder exchangeFilter;
     
     @Before
     public void setUp() throws Exception {
         tester = new ServletTester();
         tester.setContextPath("/webApp");
-        tester.addFilter(ExchangeRecordServletFilterImpl.class, "/*", 0);
-        tester.addServlet(DefaultServlet.class, "/");
+        exchangeFilter = tester.addFilter(ExchangeRecordServletFilterImpl.class, "/*", 0);
+        tester.addServlet(TestServlet.class, "/*");
         tester.start();
+        exchangeFilter.setFilter(spy(exchangeFilter.getFilter())); // Spy the filter
     }
     
     @Test
@@ -87,8 +76,6 @@ public class JettyServletFilterTest {
         HttpTester response = new HttpTester();
         request.setMethod("GET");
         request.setHeader("Host", "tester");
-        //request.setURI("/app/Person");
-        //request.setURI("/webApp");
         request.setURI("http://tester/webApp/");
         request.setVersion("HTTP/1.0");
         String generate = request.generate();
@@ -98,7 +85,11 @@ public class JettyServletFilterTest {
         assertEquals(200, response.getStatus());
         System.out.println("response Content = " + response.getContent());
         System.out.println("response URI = " + response.getURI());
-        //assertEquals("<h1>Hello Servlet</h1>", response.getContent());
+        assertEquals("Test", response.getContent());
+        verify(exchangeFilter.getFilter(), times(1)).doFilter(
+        		any(ServletRequest.class),
+        		any(ServletResponse.class),
+        		any(FilterChain.class));
     }
     
     @After
