af680d7cc4fb3a637807cf1b33b00647cdf81f29
Marwane Kalam-Alami <marwane.kalam-alami@openwide.fr>
2012-06-04 12:19:40 +0200

    Wireframe cleanup

--- easysoa-web/js/wireframe.js
+++ easysoa-web/js/wireframe.js
@@ -11,15 +11,15 @@ exports.configure = function(webServer) {
 
   webServer.set('views', settings.WWW_PATH + WIREFRAME_PATH + '/views');
 
-  webServer.get(WIREFRAME_PATH + '/style.css', function(request, result) {
-    var cssStr = fs.readFileSync(settings.WWW_PATH + WIREFRAME_PATH + '/style.styl', 'utf8');
+  webServer.get(WIREFRAME_PATH + '/css/style.css', function(request, result) {
+    var cssStr = fs.readFileSync(settings.WWW_PATH + WIREFRAME_PATH + '/css/style.styl', 'utf8');
     stylus.render(cssStr, {}, function(err, css) {
       result.end(css);
     });
   });
   webServer.get(WIREFRAME_PATH + '/*', function(request, result, next) {
     var page = request.url.replace(WIREFRAME_PATH, '').substr(1) || 'index';
-    if (page.indexOf('/') == -1 && page.indexOf('.') == -1) {
+    if (page.indexOf('/') == -1) {
       result.render(page + '.jade');
     }
     else {
