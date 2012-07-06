e7d562683624d30b3ef4a9fd3a3ac74c9441a7cd
Marc Dutoo <marc.dutoo@openwide.fr>
2012-07-06 20:29:12 +0200

    improved

--- easysoa-discovery-code/easysoa-discovery-code-sample/src/test/java/org/easysoa/samples/axxx/AxxxDpsDcvCollaborationTest.java
+++ easysoa-discovery-code/easysoa-discovery-code-sample/src/test/java/org/easysoa/samples/axxx/AxxxDpsDcvCollaborationTest.java
@@ -7,12 +7,12 @@ import org.easysoa.discovery.rest.client.DiscoveryRequest;
 import org.easysoa.discovery.rest.model.Deliverable;
 import org.easysoa.discovery.rest.model.Endpoint;
 import org.easysoa.discovery.rest.model.Environment;
+import org.easysoa.discovery.rest.model.Service;
 import org.easysoa.discovery.rest.model.ServiceImpl;
 import org.easysoa.discovery.rest.model.SoaNode;
 import org.easysoa.discovery.rest.model.SoaSystem;
 import org.easysoa.discovery.rest.model.Software;
 import org.junit.After;
-import org.junit.AfterClass;
 import org.junit.BeforeClass;
 import org.junit.Test;
 
@@ -22,20 +22,25 @@ public class AxxxDpsDcvCollaborationTest {
     private static DiscoveryRequest request;
     
     private static Environment stagingEnvironment;
-    private static Environment devDpsEnvironment;
+    private static Environment dpsDevTest1Environment;
     
     private static Software dcvCrmSoftware;
     private static Software dpsApvSoftware;
 
     private static SoaSystem axxxDcvCrmApplicationSystem;
+    private static SoaSystem axxxDpsApvApplicationSystem;
     private static SoaSystem collaborationSoaSystem;
     private static SoaSystem collaborationBusinessRequirementsSystem;
     private static SoaSystem collaborationArchitectureRequirementsSystem;
     private static SoaNode dpsTestSoapuiSystem;
+
+    private static Service clientService;
+    private static Service tdrService;
     
     private static Endpoint clientServiceEndpoint;
     private static Endpoint tdrServiceEndpoint;
     private static Endpoint clientServiceEndpoint2;
+    private static Endpoint dpsDevTest1TdrServiceEndpoint;
     private static Endpoint clientServiceMockDevDpsEndpoint;
 
     private static String clientServiceWsdl;
@@ -54,38 +59,67 @@ public class AxxxDpsDcvCollaborationTest {
     }
 
 
+
+
+
     @Test
     public void dcvCrmDeveloperDoesWebDisco() throws IOException {
+        // TODO new AxxxSoa for multitenancy
         
-        // DCV : CRM developer does a (code) web discovery of ClientService
+        // DCV : CRM developer does a web discovery of ClientService
+        // (in axxx staging env ("where is it ?"), axxxDcvCrm physical application by default ("what provides it ?") & collaboration "logical" target / SOA => services serve to collaboration ("what is it useful for ?"))
+        // NB. must say what is its target ("what is it useful for ?") to create services from endpoints NOOOOOOOOOOOOO its application
         stagingEnvironment = new Environment("staging", "0.1");
         clientServiceWsdl = "<wsdl/>";
         clientServiceEndpoint = new Endpoint(stagingEnvironment, "http://vmcrm/WS/ClientService.asmx", clientServiceWsdl, "0.1");
-        clientServiceEndpoint.addRelation(stagingEnvironment); // TODO deployedDeliverable between
-        axxxDcvCrmApplicationSystem = new SoaSystem("AXXX DCV CRM Application", "0.1");
-        axxxDcvCrmApplicationSystem.addRelation(clientServiceEndpoint); // set in web discovery ui ;TODO rather production endpoint ?!
-        // also serviceImpl / deployable, deployedDeployable ??
-        // save model...
-        // version ?
-        request.addDiscoveryNotification(stagingEnvironment, clientServiceEndpoint, axxxDcvCrmApplicationSystem);
+        clientServiceEndpoint.addRelation(stagingEnvironment); // TODO add default deployedDeliverable between
+        axxxDcvCrmApplicationSystem = new SoaSystem("AXXX DCV CRM Application", "0.1"); // TODO provided by DCV CRM
+        ////axxxDcvCrmApplicationSystem.addRelation(clientServiceEndpoint); // set in web discovery ui ; TODO rather production endpoint ?!
+        
+        // at disco end (disco save / approve) (or in validation / correlation dashboard :)
+        // "some environment elements (endpoints) are not propertized, we suggest you do"
+        //stagingEnvironment.setParameter("vmcrm", "vmcrm"); // TODO (or on deployedDeliverable ??)
+        //AxxxSoa.addParameter("vmcrm"); // or auto when previous line
+        clientServiceEndpoint.setUrl("http://${vmcrm}/WS/ClientService.asmx");
+        
+        // target env design (or below) : "is it the target env ?" yes ! then :
+        // auto add some requirements gotten from itself (wsdl)
+        clientServiceEndpoint.getRequirements().add("wsdl=" + clientServiceWsdl);
+        // (NB. if disco in dcvDev env first, not the target env, so "transition / fork & design it" (or after publish, only in collab pov)
+        // service auto creation in target soa (rather than "there is no (matched) service yet, do you want to create them ?")
+        clientService = new Service("ClientService", "0.1");
+        clientService.addRelation(clientServiceEndpoint); // TODO add default serviceimpl between
+        axxxDcvCrmApplicationSystem.addRelation(clientService); // set in web discovery ui
+        
+        // approves disco'd model by (minor) version / snapshot (or may be autosnapshotted before next disco)
+
+        // NB. target env (staging) plays the role of requirements / expected model
+        // so to see completion progress of collab, display your work in the context of your forked collab ("what's not there yet in dps (dcv)")
+        // ex. for each endpoint : impl (can be seen in code, tests), (dev or ci) test envs of this impl (can be seen by web disco...), its depl in other envs, its depl here (web disco)...
+        
+        request.addDiscoveryNotification(stagingEnvironment, clientServiceEndpoint, axxxDcvCrmApplicationSystem, clientService);
         System.out.println("\ndcvCrmDeveloperDoesWebDisco:");
     }
     
     @Test
     public void dcpOrDpsCompletesDesign() throws IOException {
+        // TODO how to share systems between requirements and actual ??
+        
         // DCP (or DPS) completes design, MAYBE also of consumption
-        collaborationBusinessRequirementsSystem = new SoaSystem("AXXX DPS DCV Collaboration BusinessProcess", "0.1");
-        // also add requirements...
-        collaborationArchitectureRequirementsSystem = new SoaSystem("AXXX DPS DCV Collaboration Architecture", "0.1");
+        collaborationBusinessRequirementsSystem = new SoaSystem("AXXX DPS DCV Collaboration BusinessProcess", "0.1"); // also add business requirements documents in it
+        collaborationArchitectureRequirementsSystem = new SoaSystem("AXXX DPS DCV Collaboration Architecture", "0.1"); // also add architecture requirements documents in it
         dcvCrmSoftware = new Software("DCV CRM", "0.1");
         collaborationArchitectureRequirementsSystem.addRelation(dcvCrmSoftware); // dcvCrmSoftware belongs to architecture
         collaborationBusinessRequirementsSystem.addRelation(dcvCrmSoftware); // dcvCrmSoftware contributes to collaborationBusinessProcessSystem
 
-        collaborationArchitectureRequirementsSystem.addRelation(clientServiceEndpoint);
-        dcvCrmSoftware.addRelation(clientServiceEndpoint); // TODO rather architectureRequirements
+        collaborationArchitectureRequirementsSystem.addRelation(clientService); // ?
+        dcvCrmSoftware.addRelation(clientService); // TODO rather architectureRequirements
         
+        // publishes (TODO major version of) model to SOA collaboration space (or / and gives read rights to DPS)
+        // TODO LATER say to those referencing the previous one that they have to update / auto update 
         collaborationSoaSystem = new SoaSystem("AXXX DPS DCV Collaboration SOA", "0.1");
-        collaborationSoaSystem.addRelation(clientServiceEndpoint); // TODO rather production endpoint ?!
+        collaborationSoaSystem.addRelation(stagingEnvironment); // models staging as "target env"
+        collaborationSoaSystem.addRelation(axxxDcvCrmApplicationSystem); // defines perimeter of sharing / publishing to collaboration // TODO or only softwares / collaborationArchitecture ??
         
         request.addDiscoveryNotification(collaborationBusinessRequirementsSystem, collaborationArchitectureRequirementsSystem,
                 dcvCrmSoftware, collaborationSoaSystem);
@@ -95,26 +129,48 @@ public class AxxxDpsDcvCollaborationTest {
     @Test
     public void dpsDesignsConsumptionAndExposition() throws IOException {
         
-        // DPS : models consumption and exposition of ClientService by (ESB) APV by (jwt process &) sca import i.e. reqs, software, endpoints
+        // DPS : models consumption and exposition of ClientService by (ESB) APV by (jwt process &) sca import i.e. reqs, software, endpoints WITHOUT wsdl
+        // (in axxx staging env & collaboration SOA)
         //Environment productionEnvironment = new Environment("prod", "0.1"); // TODO later copy to prod env
-        tdrServiceWsdl = "<wsdl/>";
-        tdrServiceEndpoint = new Endpoint(stagingEnvironment, "http://vmapv/cxf/TdrService", tdrServiceWsdl, "0.1");
+        tdrServiceEndpoint = new Endpoint(stagingEnvironment, "http://vmapv/cxf/TdrService", null, "0.1");
         tdrServiceEndpoint.addRelation(stagingEnvironment); // TODO deployedDeliverable between
+
+        // service auto creation (rather than "there is no (matched) service yet, do you want to create them ?")
+        tdrService = new Service("TdrService", "0.1");
+        tdrService.addRelation(tdrServiceEndpoint); // TODO add default serviceimpl between
+        
+        axxxDpsApvApplicationSystem = new SoaSystem("AXXX DPS APV Application", "0.1"); // TODO provided by DPS
+        axxxDpsApvApplicationSystem.addRelation(tdrService); // set in web discovery ui
         
         dpsApvSoftware = new Software("DPS APV", "0.1");
         collaborationArchitectureRequirementsSystem.addRelation(dpsApvSoftware); // dpsApvSoftware belongs to architecture
         collaborationBusinessRequirementsSystem.addRelation(dpsApvSoftware); // dpsApvSoftware contributes to collaborationBusinessProcessSystem
 
-        collaborationArchitectureRequirementsSystem.addRelation(tdrServiceEndpoint);
-        dpsApvSoftware.addRelation(tdrServiceEndpoint); // TODO rather architectureRequirements
-        
-        collaborationSoaSystem.addRelation(tdrServiceEndpoint); // TODO rather production endpoint ?!
-        
+        collaborationArchitectureRequirementsSystem.addRelation(tdrService); // ?
+        dpsApvSoftware.addRelation(tdrService); // TODO rather architectureRequirements
+
         defaultDpsApvDeliverable = new Deliverable("defaultDpsApvDeliverable", "defaultDpsApvDeliverable", "0.1", null);
         dpsApvSoftware.addRelation(defaultDpsApvDeliverable); // modeling implementation
-        defaultDpsApvDeliverable.addRelation(clientServiceEndpoint); // modeling architectural consumption
+        defaultDpsApvDeliverable.addRelation(clientService); // modeling architectural consumption
+
+        collaborationSoaSystem.addRelation(axxxDpsApvApplicationSystem); // defines perimeter of sharing / publishing to collaboration // TODO or only softwares / collaborationArchitecture or even business ??
         
-        request.addDiscoveryNotification(tdrServiceEndpoint, dpsApvSoftware,
+        // at disco end (disco save / approve) (or in validation / correlation dashboard) :
+        // "some environment elements (endpoints) are not propertized, we suggest you do (and stored jwt / sca can be also)"
+        //stagingEnvironment.setParameter("vmapv", "vmapv"); // TODO (or on deployedDeliverable ??)
+        //AxxxSoa.addParameter("vmapv"); // or auto when previous line
+        tdrServiceEndpoint.setUrl("http://${vmapv}/cxf/TdrService");
+        
+        // since target env :
+        // auto add some requirements gotten from itself (wsdl)
+        tdrServiceEndpoint.getRequirements().add("wsdl=" + "to be defined");
+        
+        // approves disco'd model by (minor) version / snapshot (or may be autosnapshotted before next disco)
+        
+        // publishes (TODO major version of) model to SOA collaboration space (or / and gives read rights to DPS)
+        // TODO LATER say to those referencing the previous one that they have to update / auto update 
+        
+        request.addDiscoveryNotification(tdrServiceEndpoint, tdrService, axxxDpsApvApplicationSystem, dpsApvSoftware,
                 collaborationBusinessRequirementsSystem, collaborationArchitectureRequirementsSystem,
                 collaborationSoaSystem, defaultDpsApvDeliverable);
         System.out.println("\ndpsDesignsConsumptionAndExposition:");
@@ -122,10 +178,15 @@ public class AxxxDpsDcvCollaborationTest {
     
     @Test
     public void dcvDesignsConsumption() throws IOException {
+        // TOOD LATER updates / get collaboration model latest (major) version
+        
         // DCV : maybe only now design consumption of CRM Client Service : download, modify and reimport sca (not code discovery because .NET) 
         defaultDcvCrmDeliverable = new Deliverable("defaultDcvCrmDeliverable", "defaultDcvCrmDeliverable", "0.1", null);
         dcvCrmSoftware.addRelation(defaultDcvCrmDeliverable); // modeling implementation
-        defaultDcvCrmDeliverable.addRelation(tdrServiceEndpoint); // modeling architectural consumption
+        defaultDcvCrmDeliverable.addRelation(tdrService); // modeling architectural consumption
+        
+        // publishes (TODO major version of) model to SOA collaboration space (or / and gives read rights to DPS)
+        // TODO LATER say to those referencing the previous one that they have to update / auto update 
 
         request.addDiscoveryNotification(defaultDcvCrmDeliverable, dcvCrmSoftware);
         System.out.println("\ndcvDesignsConsumption:");
@@ -133,6 +194,7 @@ public class AxxxDpsDcvCollaborationTest {
     
     @Test
     public void dpsDiscoversCodeThenWeb() throws IOException {
+        // TOOD LATER updates / get collaboration model latest (major) version
         
         // DPS : (ESB) APV developer does a code discovery of Client Service consumption & exposition
         
@@ -145,13 +207,32 @@ public class AxxxDpsDcvCollaborationTest {
         dpsApvTdrDeliverable.addRelation(dpsApvTdrServiceImpl);
         dpsApvTdrDeliverable.addRequirement("Consumes WS of JAX-WS interface org.easysoa.samples.axxx.dcv.ClientService");
         
-        // create / find service & link them
-        // TODO both services with same wsdl (ClientService's), or TdrService is different but where comes his wsdl from (java2wsdl ?) ?
-        
-        // web disco
-        tdrServiceEndpoint.setWsdl(tdrServiceWsdl); // TODO store actual wsdl in another endpoint (ex. in "design" environment ??)
+        // in validation / correlation dashboard, find corresponding service & link them :
+        tdrService.addRelation(dpsApvTdrServiceImpl);
+        //// OBSOLETE Q both services with same wsdl (ClientService's), or TdrService is different but where comes his wsdl from (java2wsdl ?) ?
 
-        request.addDiscoveryNotification(dpsApvTdrDeliverable, dpsApvTdrServiceImpl);
+        // DCV : DPS developer does a web discovery of TdrService
+        // (in dpsDevTest1 env ("where is it ?"), axxxDpsApv physical application by default ("what provides it ?") & collaboration "logical" target / SOA => services serve to collaboration ("what is it useful for ?"))
+        // NB. must say what is its target ("what is it useful for ?") to create services from endpoints NOOOOOOOOOOOOO its application
+        tdrServiceWsdl = "<wsdl/>";
+        dpsDevTest1Environment = new Environment("dpsDevTest1", "0.1"); // with vmcrm & vmapv params since belongs to AxxxSoa
+        dpsDevTest1TdrServiceEndpoint = new Endpoint(dpsDevTest1Environment, "http://${vmapv}/cxf/TdrService", tdrServiceWsdl, "0.1"); // auto prop'd with known params
+        dpsDevTest1TdrServiceEndpoint.addRelation(dpsDevTest1Environment); // TODO add known deployedDeliverable between
+        // since tdrService belongs to axxxDpsApvApplicationSystem which has been set in web discovery ui, and since WSDL matches with target env's (expected one), service can be auto found :
+        tdrService.addRelation(dpsDevTest1TdrServiceEndpoint); // TODO add known serviceimpl between
+        
+        // contribute requirements from known :
+        tdrServiceEndpoint.getRequirements().add("wsdl=" + dpsDevTest1TdrServiceEndpoint.getWsdl()); 
+        
+        // web disco in staging
+        tdrServiceEndpoint.setWsdl(tdrServiceWsdl); // TODO store actual wsdl in another endpoint (ex. in "design" environment ??)
+        
+        // TODO DPS impl mock clientService, matches expected (gotten from target env) wsdl in dev !!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
+
+        // publishes (TODO major version of) model to SOA collaboration space (or / and gives read rights to DPS)
+        // TODO LATER say to those referencing the previous one that they have to update / auto update
+        
+        request.addDiscoveryNotification(dpsApvTdrDeliverable, dpsApvTdrServiceImpl, dpsDevTest1Environment, dpsDevTest1TdrServiceEndpoint);
         System.out.println("\ndpsDiscoversCode:");
     }
     
@@ -177,7 +258,7 @@ public class AxxxDpsDcvCollaborationTest {
         // webdisco.reinit(); // TODO !
         clientServiceEndpoint2 = new Endpoint(stagingEnvironment, "http://vmcrm/WS/ClientService.asmx", clientServiceWsdl, "0.1");
         clientServiceEndpoint2.addRelation(stagingEnvironment); // TODO deployedDeliverable between
-        axxxDcvCrmApplicationSystem.addRelation(clientServiceEndpoint); // set in web discovery ui ;TODO rather production endpoint ?!
+        axxxDcvCrmApplicationSystem.addRelation(clientServiceEndpoint); // set in web discovery ui ; TODO rather production endpoint ?!
         //if (clientService has no more endpoint in staging) { error("Staging : no (more) endpoint for clientService in staging. Not ready to version.")}
         //if (clientServiceEndpoint2 has no related service) { warn("New service, do you want : to link it with existing [suggestions], or to put it in SOAs [SOA systems] ?"); }
 
@@ -187,17 +268,17 @@ public class AxxxDpsDcvCollaborationTest {
     
     @Test
     public void dpsRediscoversOldClientServiceInDevSoapui() throws IOException {
-        devDpsEnvironment = new Environment("devDps", "0.1");
-        clientServiceMockDevDpsEndpoint = new Endpoint(devDpsEnvironment, "http://vmcrm/WS/ClientService.asmx", clientServiceWsdl, "0.1");
-        clientServiceMockDevDpsEndpoint.addRelation(devDpsEnvironment); // TODO deployedDeliverable between
+        dpsDevTest1Environment = new Environment("devDps", "0.1");
+        clientServiceMockDevDpsEndpoint = new Endpoint(dpsDevTest1Environment, "http://vmcrm/WS/ClientService.asmx", clientServiceWsdl, "0.1");
+        clientServiceMockDevDpsEndpoint.addRelation(dpsDevTest1Environment); // TODO deployedDeliverable between
         dpsTestSoapuiSystem = new SoaSystem("dpsTestSoapui", "0.1");
-        dpsTestSoapuiSystem.addRelation(clientServiceMockDevDpsEndpoint); // set in web discovery ui ;TODO rather production endpoint ?!
+        dpsTestSoapuiSystem.addRelation(clientServiceMockDevDpsEndpoint); // set in web discovery ui ; TODO rather production endpoint ?!
 
         // on revalidation, alerts DPS about inconsistency :
-        // web discovery (in dev) :
+        // web discovery (in dev) : redisco'd dev env wsdl differs from reference TODO code NO or design (is old one, found by hash) 
         if (!clientServiceMockDevDpsEndpoint.getWsdl().equals(clientServiceEndpoint.getWsdl())) { System.out.println("Error : web disco'd test wsdl differs from the design one");}
         
-        request.addDiscoveryNotification(devDpsEnvironment, clientServiceMockDevDpsEndpoint, dpsTestSoapuiSystem);
+        request.addDiscoveryNotification(dpsDevTest1Environment, clientServiceMockDevDpsEndpoint, dpsTestSoapuiSystem);
         System.out.println("\ndpsRediscoversOldClientServiceInDev:");
     }
     
