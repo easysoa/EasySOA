4e33f2d4b3d94643cbf8e1556760213bbd48e570
Yoann Rodière <yoann.rodiere@openwide.fr>
2012-07-11 12:27:59 +0200

    First test version

--- easysoa-registry-query/deploy.sh
+++ easysoa-registry-query/deploy.sh
@@ -6,13 +6,19 @@ NUXEO_INSTANCE_PLUGINS_DIR="$NUXEO_INSTANCE_HOME/nxserver/plugins"
 UI="easysoa-registry-query-nuxeo-contentviews"
 TESTMODEL="easysoa-registry-query-nuxeo-test-model"
 
+show_status () {
+	echo 1>&2 "Deployment on existing Nuxeo instance
+  * Path:	$NUXEO_INSTANCE_HOME
+  * Result:	$1"
+}
+
+DEPLOY_MSG=
 if cp -t "$NUXEO_INSTANCE_PLUGINS_DIR" \
 	"$UI/target"/*.jar "$TESTMODEL/target/"*.jar
 then
-	echo "OK"
+	show_status "OK"
 	exit 0
 else
-	echo "Failure"
-	exit 1
+	show_status "FAILED"
 fi
 
