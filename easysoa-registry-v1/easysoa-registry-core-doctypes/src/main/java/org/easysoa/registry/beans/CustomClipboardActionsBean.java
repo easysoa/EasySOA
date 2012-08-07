41d0ae49b4d3c6189c79600dd31b71cf5212b1a8
Marwane Kalam-Alami <marwane.kalam-alami@openwide.fr>
2012-08-07 15:39:01 +0200

    Make parent documents appear in document view

---
+++ easysoa-registry-v1/easysoa-registry-core-doctypes/src/main/java/org/easysoa/registry/beans/CustomClipboardActionsBean.java
@@ -0,0 +1,5 @@
+package org.easysoa.registry.beans;
+
+public class CustomClipboardActionsBean {
+
+}
--- easysoa-registry-v1/easysoa-registry-core-doctypes/src/main/java/org/easysoa/registry/beans/DocumentModelHelperBean.java
+++ easysoa-registry-v1/easysoa-registry-core-doctypes/src/main/java/org/easysoa/registry/beans/DocumentModelHelperBean.java
@@ -3,10 +3,14 @@ package org.easysoa.registry.beans;
 import org.apache.commons.logging.Log;
 import org.apache.commons.logging.LogFactory;
 import org.jboss.seam.ScopeType;
+import org.jboss.seam.annotations.In;
 import org.jboss.seam.annotations.Install;
 import org.jboss.seam.annotations.Name;
 import org.jboss.seam.annotations.Scope;
+import org.nuxeo.ecm.core.api.CoreSession;
 import org.nuxeo.ecm.core.api.DocumentModel;
+import org.nuxeo.ecm.core.api.DocumentModelList;
+import org.nuxeo.ecm.core.api.impl.DocumentModelListImpl;
 import org.nuxeo.ecm.platform.types.TypeManager;
 import org.nuxeo.runtime.api.Framework;
 
@@ -15,6 +19,8 @@ import org.nuxeo.runtime.api.Framework;
 @Install(precedence = Install.FRAMEWORK)
 public class DocumentModelHelperBean {
 
+    @In CoreSession documentManager;
+    
     private static final Log log = LogFactory.getLog(DocumentModelHelperBean.class);
 
     public String getDocumentTypeLabel(DocumentModel model) throws Exception {
@@ -25,6 +31,28 @@ public class DocumentModelHelperBean {
             log.warn("Failed to fetch document type label, falling back to type name instead.");
             return model.getType();
         }
+    }
+    
+    public DocumentModelList getParents(DocumentModel documentModel) throws Exception {
+        // Find working copy of document
+        DocumentModel workingModel;
+        if (documentModel.isProxy()) {
+            workingModel = documentManager.getWorkingCopy(documentModel.getRef());
+        }
+        else {
+            workingModel = documentModel;
+        }
+
+        // Build proxies list
+        DocumentModelList modelInstances = documentManager.getProxies(documentModel.getRef(), null);
+        modelInstances.add(workingModel);
+        
+        // Fetch parents
+        DocumentModelList parents = new DocumentModelListImpl();
+        for (DocumentModel modelInstance : modelInstances) {
+            parents.add(documentManager.getParentDocument(modelInstance.getRef()));
+        }
+        return parents;
     }
 
 }
