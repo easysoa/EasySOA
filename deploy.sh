1bbbbf211b51e470dede7c682a4196c5a7e1a05e
Marc Dutoo <marc.dutoo@openwide.fr>
2012-10-11 18:36:13 +0200

    improved deps copy

--- deploy.sh
+++ deploy.sh
@@ -8,5 +8,5 @@ export M2_REPO=/cygdrive/c/Documents\ and\ Settings/mdutoo/.m2/repository
 cp easysoa-registry-v1/*/target/*jar $NUXEO_HOME/nxserver/plugins/
 cp "$M2_REPO/net/sf/jung/jung-api/2.0.1/jung-api-2.0.1.jar" "$NUXEO_HOME/lib"
 cp "$M2_REPO/net/sf/jung/jung-graph-impl/2.0.1/jung-graph-impl-2.0.1.jar" "$NUXEO_HOME/lib"
-cp "$M2_REPO/net/sf/jung/jung-algorithms/2.0.1/jung-algorithms-2.0.1.jar" /cygdrive/c/dev/easysoa/nuxeo-cap-5.5-tomcat/lib/
+cp "$M2_REPO/net/sf/jung/jung-algorithms/2.0.1/jung-algorithms-2.0.1.jar" $NUXEO_HOME/lib/
 cp "$M2_REPO/net/sourceforge/collections/collections-generic/4.01/collections-generic-4.01.jar" "$NUXEO_HOME/lib"
