94d905abef611bb76722f85c98c618d9e2a61511
Jeremie Guillemotte <jeremie.guillemotte@openwide.fr>
2013-06-28 12:41:01 +0200

    Add convinience methods to get the list of header names and the value for a specific header

--- easysoa-registry/easysoa-registry-api/easysoa-registry-api-messaging/src/main/java/org/easysoa/message/Headers.java
+++ easysoa-registry/easysoa-registry-api/easysoa-registry-api-messaging/src/main/java/org/easysoa/message/Headers.java
@@ -1,20 +1,20 @@
 /**
  * EasySOA Proxy
  * Copyright 2011 Open Wide
- * 
+ *
  * This program is free software: you can redistribute it and/or modify
  * it under the terms of the GNU Lesser General Public License as published by
  * the Free Software Foundation, either version 3 of the License, or
  * (at your option) any later version.
- * 
+ *
  * This program is distributed in the hope that it will be useful,
  * but WITHOUT ANY WARRANTY; without even the implied warranty of
  * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
  * GNU Lesser General Public License for more details.
- * 
+ *
  * You should have received a copy of the GNU Lesser General Public License
  * along with this program.  If not, see <http://www.gnu.org/licenses/>.
- * 
+ *
  * Contact : easysoa-dev@googlegroups.com
  */
 
@@ -26,7 +26,7 @@ import java.util.List;
 /**
  * Header collection
  * @author jguillemotte
- * 
+ *
  */
 public class Headers {
 
@@ -46,6 +46,32 @@ public class Headers {
 	public void addHeader(Header header) {
 		headerList.add(header);
 	}
+
+    /**
+     * Return the value of the header
+     * @param headerName The Header name (not case sensitive)
+     * @return The header value, null if the header name is not found
+     */
+    public String getHeaderValue(String headerName){
+        for(Header header : headerList){
+            if(header.getName().equalsIgnoreCase(headerName)){
+                return header.getValue();
+            }
+        }
+        return null;
+    }
+
+    /**
+     * Returns a list of header names
+     * @return A list containing the header names
+     */
+    public List<String> getHeaderNames(){
+        List<String> headerNameList = new ArrayList<String>();
+        for(Header header : headerList){
+            headerNameList.add(header.getName());
+        }
+        return headerNameList;
+    }
 
 	/**
 	 * Returns the headers.
